cda8f97c226d9a58dc04508444dc5d773ef8a71b
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-05-04 14:28:07 +0200

    Set up JEE-Application with example Rest Interface Includes example unit test

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/RestConfig.java
@@ -0,0 +1,9 @@
+package de.fau.amos.virtualledger.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/*")
+public class RestConfig extends Application {
+
+}
---
+++ server/src/main/java/de/fau/amos/virtualledger/server/TestRestProvider.java
@@ -0,0 +1,14 @@
+package de.fau.amos.virtualledger.server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("test")
+public class TestRestProvider {
+
+    @GET
+    public Response helloWorld() {
+        return Response.ok("Hello World").build();
+    }
+}
