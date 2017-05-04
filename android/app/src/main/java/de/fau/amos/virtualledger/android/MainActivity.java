1ec691f67c626558efbf742b53b801385e3d38be
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-05-04 14:27:17 +0200

    Setup of Hello World Android app

---
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/MainActivity.java
@@ -0,0 +1,15 @@
+package de.fau.amos.virtualledger.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.fau.amos.virtualledger.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
