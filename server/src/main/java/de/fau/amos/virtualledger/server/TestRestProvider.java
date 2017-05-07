f0532fbac839e8d3bc90b6e7aeaf3b265ff3da83
Rami Mahfoud <rami.mahfoud88@gmail.com>
2017-05-08 01:06:00 +0200

    Fix Test

--- server/src/main/java/de/fau/amos/virtualledger/server/TestRestProvider.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/TestRestProvider.java
@@ -9,7 +9,7 @@ public class TestRestProvider {
 
     @GET
     public Response helloWorld() {
-        return Response.ok("Hello World With Travis CI Test").build();
+        return Response.ok("Hello World").build();
     }
 }
  
