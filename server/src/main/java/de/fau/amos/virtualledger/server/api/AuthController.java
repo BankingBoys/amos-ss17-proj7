1829eca71b29a9772a548db9e36bf69855028056
Georg <georg.schwarz@fau.de>
2017-05-07 13:24:44 +0200

    - implemented model class UserCredential.java - tested setters for UserCredential.java with pattern comparison in UserCredentialTest.java - added REST endpoint /api/auth/register that parses json object to UserCredential
    - ATTENTION: does not work with Glassfish 4.1.2 due some bugs, please use Glassfish 4.1.0 instead
    
    Signed-off-by: Georg <georg.schwarz@fau.de>

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/AuthController.java
@@ -0,0 +1,31 @@
+package de.fau.amos.virtualledger.server.api;
+
+import de.fau.amos.virtualledger.server.model.UserCredential;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Georg on 07.05.2017.
+ */
+@Path("/auth")
+public class AuthController {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/register")
+    public Response register(UserCredential credential) {
+
+        if(credential == null || credential.getEmail() == null || credential.getPassword() == null)
+        { // if not null, matches the pattern -> specified in model class
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        // TODO call bean that inserts the credential into the database if email isn't used yet
+        return Response.ok("You were registered! " + credential.getEmail()).build();
+    }
+}
---
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/UserCredential.java
@@ -0,0 +1,62 @@
+package de.fau.amos.virtualledger.server.model;
+
+
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Georg on 07.05.2017.
+ */
+@XmlRootElement
+public class UserCredential {
+
+    public String getEmail() {
+        return email;
+    }
+
+    @XmlElement(name="email")
+    public void setEmail(String email) {
+
+        if(email == null || ! this.isEmailPatternValid(email))
+        {
+            throw new IllegalArgumentException("Email doesn't match the Email-Pattern");
+        }
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @XmlElement(name="password")
+    public void setPassword(String password) {
+
+        if(password == null || ! isPasswordPatternValid(password))
+        {
+            throw new IllegalArgumentException("Password must not be empty!");
+        }
+        this.password = password;
+    }
+
+    private String email;
+    private String password;
+
+    public UserCredential() {}
+
+    private boolean isEmailPatternValid(String email)
+    {
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean isPasswordPatternValid(String password)
+    {
+        return ! password.isEmpty();
+    }
+}
