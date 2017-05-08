ae9024335244f3701f97ded274bfe7a74affe7f6
Georg <georg.schwarz@fau.de>
2017-05-08 15:18:01 +0200

    - added UserCredentialRepository.java for operations on UserCredentials in database

--- server/src/main/java/de/fau/amos/virtualledger/server/api/AuthController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/AuthController.java
@@ -1,7 +1,9 @@
 package de.fau.amos.virtualledger.server.api;
 
 import de.fau.amos.virtualledger.server.model.UserCredential;
+import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
@@ -17,6 +19,9 @@ import javax.ws.rs.core.Response;
 @Path("/auth")
 public class AuthController {
 
+    @Inject
+    UserCredentialRepository userCredentialRepository;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/register")
@@ -24,14 +29,14 @@ public class AuthController {
 
         if(credential == null || credential.getEmail() == null || credential.getPassword() == null)
         { // if not null, matches the pattern -> specified in model class
-            return Response.status(Response.Status.BAD_REQUEST).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("Please check your inserts! At least one was not formatted correctly!").build();
+        }
+        if(this.userCredentialRepository.existsUserCredentialEmail(credential.getEmail()))
+        {
+            return Response.status(Response.Status.BAD_REQUEST).entity("There already exists an account with this Email address.").build();
         }
 
-        EntityManager entityManager = Persistence.createEntityManagerFactory("auth-db").createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(credential);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        this.userCredentialRepository.createUserCredential(credential);
 
         // TODO call bean that inserts the credential into the database if email isn't used yet
         return Response.ok("You were registered! " + credential.getEmail()).build();
