a64d8da5704082418a829512b34da56b19c5dd04
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-14 17:52:00 +0200

    added login as activitie

---
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/LoginActivity.java
@@ -0,0 +1,20 @@
+package de.fau.amos.virtualledger.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import de.fau.amos.virtualledger.R;
+
+/**
+ * Created by sebastian on 14.05.17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
--- android/app/src/main/java/de/fau/amos/virtualledger/android/MainActivity.java
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 import de.fau.amos.virtualledger.R;
 
@@ -17,16 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
 
         Button button_register = (Button) findViewById(R.id.button_register);
-
+        final MainActivity _this = this;
         button_register.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        setContentView(R.layout.login);
+
+                        Intent intent = new Intent(_this, LoginActivity.class);
+                        startActivity(intent);
                     }
                 }
         );
