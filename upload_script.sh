9acb3acef2730db212168c05ce81095ea699eb78
Rami Mahfoud <rami.mahfoud88@gmail.com>
2017-05-14 19:23:05 +0200

    Secure connection with Travis ci

--- upload_script.sh
+++ upload_script.sh
@@ -12,7 +12,7 @@
       -F "notes=auto deploy." \
       -F "notes_type=0" \
       -F "ipa=@app-debug.apk" \
-      -H "X-HockeyAppToken: 788d6d4924ae4144a20601938fb9354e" \
+      -H "X-HockeyAppToken: $hockeyapptoken" \
       https://rink.hockeyapp.net/api/2/apps/4adac746746644d8ad6c94939495c7ff//app_versions/upload
 
 
