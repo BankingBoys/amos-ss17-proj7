07f746a92fe4a6033eb620b677627abbc30a0fea
simonschmoll <simon.schmoll@gmail.com>
2017-05-14 14:12:41 +0200

    - created RestApi in UI for logout - created new model class

---
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/api/model/LogoutApiModel.java
@@ -0,0 +1,36 @@
+package de.fau.amos.virtualledger.android.api.model;
+
+/**
+ * Created by Simon on 14.05.2017.
+ */
+
+public class LogoutApiModel {
+    private String email;
+
+    /**
+     *
+     * @param email
+     * @methodtype constructor
+     */
+    public LogoutApiModel(String email) {
+        this.email = email;
+    }
+
+    /**
+     *
+     * @return email
+     * @methodtype getter
+     */
+    public String getemail() {
+        return email;
+    }
+
+    /**
+     *
+     * @param email
+     * @methodtype setter
+     */
+    public void setemail(String email) {
+        this.email = email;
+    }
+}
