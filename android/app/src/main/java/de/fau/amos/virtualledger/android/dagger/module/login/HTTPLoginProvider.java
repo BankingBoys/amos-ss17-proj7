954295ba6317ca5f5fe23d53c95804ddd08fabb6
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-14 19:49:46 +0200

    Implemented HTTP LiginProvider

--- android/app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/login/HTTPLoginProvider.java
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/login/HTTPLoginProvider.java
@@ -1,13 +1,26 @@
 package de.fau.amos.virtualledger.android.dagger.module.login;
 
+import de.fau.amos.virtualledger.android.api.Restapi;
+import de.fau.amos.virtualledger.android.model.UserCredential;
+import de.fau.amos.virtualledger.dtos.LoginData;
+import de.fau.amos.virtualledger.dtos.StringApiModel;
+import retrofit2.Retrofit;
+
 /**
  * Created by sebastian on 14.05.17.
  */
 
 public class HTTPLoginProvider implements LoginProvider {
+    private Retrofit retrofit;
+    private String token =  "";
+
+    public HTTPLoginProvider(Retrofit retrofit){
+        this.retrofit = retrofit;
+    }
+
     @Override
     public void login(String username, String password) {
-
+        retrofit2.Call<StringApiModel> responseMessage = retrofit.create(Restapi.class).login(new LoginData(username,password));
     }
 
     @Override
@@ -17,12 +30,12 @@ public class HTTPLoginProvider implements LoginProvider {
 
     @Override
     public boolean isLoggedIn() {
-        return false;
+        return token.length()==0;
     }
 
     @Override
     public String getToken() {
-        return null;
+        return this.token;
     }
 
     @Override
