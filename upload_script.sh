b7d4f5ee2bae814af0eb424964c41b898dd47175
Rami Mahfoud <rami.mahfoud88@gmail.com>
2017-05-14 18:39:01 +0200

    Fix apk erro

--- upload_script.sh
+++ upload_script.sh
@@ -11,7 +11,7 @@
       -F "notify=1" \
       -F "notes=auto deploy." \
       -F "notes_type=0" \
-      -F "ipa=@app-release-unsigned.apk" \
+      -F "ipa=@app-debug.apk" \
       -H "X-HockeyAppToken: 788d6d4924ae4144a20601938fb9354e" \
       https://rink.hockeyapp.net/api/2/apps/4adac746746644d8ad6c94939495c7ff//app_versions/upload
 
