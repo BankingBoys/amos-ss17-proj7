e98afc911ff9d1b662e45ef1c969347890b0ac5d
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-14 23:24:45 +0200

    added server-response handling

--- android/app/src/main/java/de/fau/amos/virtualledger/android/auth/login/HTTPLoginProvider.java
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/auth/login/HTTPLoginProvider.java
@@ -1,9 +1,19 @@
 package de.fau.amos.virtualledger.android.auth.login;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
+
+import java.io.IOException;
+
+import de.fau.amos.virtualledger.android.MainActivity_Menu;
+import de.fau.amos.virtualledger.android.RegisterActivity;
 import de.fau.amos.virtualledger.android.api.Restapi;
 import de.fau.amos.virtualledger.android.model.UserCredential;
 import de.fau.amos.virtualledger.dtos.LoginData;
 import de.fau.amos.virtualledger.dtos.StringApiModel;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 /**
@@ -21,6 +31,20 @@ public class HTTPLoginProvider implements LoginProvider {
     @Override
     public void login(String username, String password) {
         retrofit2.Call<StringApiModel> responseMessage = retrofit.create(Restapi.class).login(new LoginData(username,password));
+
+        responseMessage.enqueue(new Callback<StringApiModel>() {
+            @Override
+            public void onResponse(retrofit2.Call<StringApiModel> call, Response<StringApiModel> response) {
+                if(response.isSuccessful()) {
+                    token = response.message();
+                }
+            }   @Override
+            public void onFailure(retrofit2.Call<StringApiModel> call, Throwable t) {
+                
+            }
+        }
+
+        );
     }
 
     @Override
