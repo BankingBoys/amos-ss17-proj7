8653e8c956bc9588b39defa5ea85736aacc651f4
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-05-15 17:55:48 +0200

    Add default constructor required by JAXB

--- dtos/src/main/java/de/fau/amos/virtualledger/dtos/LoginData.java
+++ dtos/src/main/java/de/fau/amos/virtualledger/dtos/LoginData.java
@@ -5,9 +5,12 @@ public class LoginData {
     public String email;
     public String password;
 
-    public LoginData(String email, String password){
-	this.email = email;
-	this.password = password;
-	}
+    public LoginData() {
+    }
+
+    public LoginData(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
 }
