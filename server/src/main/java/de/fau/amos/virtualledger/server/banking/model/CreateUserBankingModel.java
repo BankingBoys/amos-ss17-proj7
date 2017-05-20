e4382b6a5c1bd94d97a8f310a493ce57a8192862
Georg <georg.schwarz@fau.de>
2017-05-20 18:55:05 +0200

    - reordered call of adorsys api on register

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/model/CreateUserBankingModel.java
@@ -0,0 +1,20 @@
+package de.fau.amos.virtualledger.server.banking.model;
+
+/**
+ * Created by Georg on 20.05.2017.
+ */
+public class CreateUserBankingModel {
+
+    private String id;
+
+    public CreateUserBankingModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
