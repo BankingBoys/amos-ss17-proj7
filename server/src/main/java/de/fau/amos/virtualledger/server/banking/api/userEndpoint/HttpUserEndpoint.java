33007ee96cecc5985ca8e197901b9a39df61ed06
Georg <georg.schwarz@fau.de>
2017-05-20 19:03:40 +0200

    - removed wrong imports

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/api/userEndpoint/HttpUserEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/api/userEndpoint/HttpUserEndpoint.java
@@ -1,10 +1,5 @@
 package de.fau.amos.virtualledger.server.banking.api.userEndpoint;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
 import de.fau.amos.virtualledger.server.banking.api.BankingApiUrlProvider;
 import de.fau.amos.virtualledger.server.banking.model.CreateUserBankingModel;
 
