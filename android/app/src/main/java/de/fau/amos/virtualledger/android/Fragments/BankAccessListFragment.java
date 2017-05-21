2ddd113a9d7a2679fec03ed9c8b9c73facdbfe77
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-21 12:56:53 +0200

    added simple example long click listener to bank access list fragment

--- android/app/src/main/java/de/fau/amos/virtualledger/android/Fragments/BankAccessListFragment.java
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/Fragments/BankAccessListFragment.java
@@ -5,14 +5,16 @@ import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import de.fau.amos.virtualledger.android.ListView.Adapter.BankAccessListAdapter;
 import de.fau.amos.virtualledger.dtos.BankAccess;
-
-import android.support.annotation.Nullable;
 
 /**
  * Created by Simon on 20.05.2017.
@@ -24,8 +26,9 @@ public class BankAccessListFragment extends ListFragment implements LoaderManage
 
 
     //For Test Purposes fake BankAccesses
-    BankAccess test = new BankAccess(0, "hallo", 0);
-    BankAccess test2 = new BankAccess(0, "hallo2", 0);
+
+    BankAccess test = new BankAccess("0", "hallo");
+    BankAccess test2 = new BankAccess("1", "hallo2");
     List<BankAccess> testList = new ArrayList<BankAccess>();
 
 
@@ -45,6 +48,15 @@ public class BankAccessListFragment extends ListFragment implements LoaderManage
         bankAccessAdapter = new BankAccessListAdapter(getActivity(), testList);
         setListAdapter(bankAccessAdapter);
 
+        getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int arg2, long arg3) {
+                Toast.makeText(getActivity(), "On long click listener", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
     }
 
     @Override
