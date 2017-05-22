9748f6805e62dcf2c84cdecb317d252407eb6835
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-05-22 14:52:15 +0200

    Butterknife

--- android/app/src/main/java/de/fau/amos/virtualledger/android/AddBankAccessActivity.java
+++ android/app/src/main/java/de/fau/amos/virtualledger/android/AddBankAccessActivity.java
@@ -2,12 +2,27 @@ package de.fau.amos.virtualledger.android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import de.fau.amos.virtualledger.R;
 
 public class AddBankAccessActivity extends AppCompatActivity {
 
     private static final String TAG = "AddBankAccessActivity";
+
+    @BindView(R.id.editText_addBankAccess_blz)
+    EditText blzEditText;
+    @BindView(R.id.editText_addBankAccess_loginName)
+    EditText loginNameEditText;
+    @BindView(R.id.editText_addBankAccess_pin)
+    EditText pinEditText;
+
+    @OnClick(R.id.button_addBankAccess_submit)
+    void submit() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
