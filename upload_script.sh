bf4acf6ebdd13ecb250d7a809ad1e1551ac2c33e
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-26 23:38:59 +0200

    hockey app extracted in file for commit only when master build

---
+++ upload_script.sh
@@ -0,0 +1,15 @@
+  #!/bin/bash
+
+   cd /home/travis/build/BankingBoys/amos-ss17-proj7/
+   ./gradlew android:app:assemble
+   cd android/app/build/outputs/apk
+   curl \
+      -F "status=2" \
+      -F "notify=1" \
+      -F "notes=auto deploy." \
+      -F "notes_type=0" \
+      -F "ipa=@app-debug.apk" \
+      -H "X-HockeyAppToken: $hockeyapptoken" \
+      https://rink.hockeyapp.net/api/2/apps/4adac746746644d8ad6c94939495c7ff//app_versions/upload
+
+
