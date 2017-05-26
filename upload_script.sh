5ba3ede606207d3bd30015f67801680e84f47fce
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-26 23:06:45 +0200

    simplified uploadscript

--- upload_script.sh
+++ upload_script.sh
@@ -1,19 +1,7 @@
   #!/bin/bash
-
    cd /home/travis/build/BankingBoys/amos-ss17-proj7/android
-   gradle -b wrapper.gradle wrapper
-   ./gradlew assemble
-   cd app
-   cd build
-   cd outputs
-   cd apk
-   curl \
-      -F "status=2" \
-      -F "notify=1" \
-      -F "notes=auto deploy." \
-      -F "notes_type=0" \
-      -F "ipa=@app-debug.apk" \
-      -H "X-HockeyAppToken: $hockeyapptoken" \
-      https://rink.hockeyapp.net/api/2/apps/4adac746746644d8ad6c94939495c7ff//app_versions/upload
+   ./../gradlew assemble
+   cd app/build/outputs/apk
+   curl -F "status=2" -F "notify=1" -F "notes=auto deploy." -F "notes_type=0" -F "ipa=@app-debug.apk" -H "X-HockeyAppToken: $hockeyapptoken" https://rink.hockeyapp.net/api/2/apps/4adac746746644d8ad6c94939495c7ff//app_versions/upload
 
 
