df54f9f48962db99de7658b27acebc93a0f58104
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-27 21:43:21 +0200

    added example test as requested

---
+++ dtos/src/test/java/dtos/TestTest.java
@@ -0,0 +1,15 @@
+package dtos;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		assertThat(Boolean.TRUE).isTrue();
+	}
+
+}
