2c4a4913c01a491c136b8aac8156f937ee735811
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-05-28 22:21:23 +0200

    changed hockey app account

--- upload_script.sh
+++ upload_script.sh
@@ -10,10 +10,11 @@ cd android/app/build/outputs/apk
 curl \
       -F "status=2" \
       -F "notify=1" \
-      -F "notes=auto deploy." \
+      -F "notes=auto deploy of "$TRAVIS_BRANCH \
       -F "notes_type=0" \
       -F "ipa=@app-debug.apk" \
       -H "X-HockeyAppToken: $hockeyapptoken" \
-      https://rink.hockeyapp.net/api/2/apps/4adac746746644d8ad6c94939495c7ff//app_versions/upload
+      https://rink.hockeyapp.net/api/2/apps/upload
+
 
 
