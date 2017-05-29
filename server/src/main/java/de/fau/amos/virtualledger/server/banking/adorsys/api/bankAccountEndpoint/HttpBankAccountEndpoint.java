e6c2c986cb20ad256ce3160c85c4ad6f520b6d35
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-05-29 17:20:33 +0200

    Workaround for Adorsys API requiring String without quotes: Pretend it isn't JSON

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/bankAccountEndpoint/HttpBankAccountEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/bankAccountEndpoint/HttpBankAccountEndpoint.java
@@ -1,25 +1,24 @@
 package de.fau.amos.virtualledger.server.banking.adorsys.api.bankAccountEndpoint;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Default;
-import javax.inject.Inject;
-import javax.ws.rs.core.MediaType;
-
 import com.sun.istack.logging.Logger;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.client.filter.LoggingFilter;
 import com.sun.jersey.api.json.JSONConfiguration;
-
 import de.fau.amos.virtualledger.server.banking.adorsys.api.BankingApiUrlProvider;
 import de.fau.amos.virtualledger.server.banking.adorsys.api.json.BankAccountJSONBankingModel;
 import de.fau.amos.virtualledger.server.banking.model.BankAccountBankingModel;
 import de.fau.amos.virtualledger.server.banking.model.BankingException;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Georg on 18.05.2017.
@@ -63,14 +62,11 @@ public class HttpBankAccountEndpoint implements BankAccountEndpoint {
     public void syncBankAccount(String userId, String bankAccessId, String bankAccountId, String pin) throws BankingException {
 
         // Create Jersey client
-        ClientConfig clientConfig = new DefaultClientConfig();
-        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-        Client client = Client.create(clientConfig);
+        Client client = Client.create();
+        client.addFilter(new LoggingFilter(System.out));
 
         String url = urlProvider.getBankAccountSyncEndpointUrl(userId, bankAccessId, bankAccountId);
-        WebResource.Builder webResourceGET = client.resource(url)
-                .accept(MediaType.APPLICATION_JSON)
-                .type(MediaType.APPLICATION_JSON);
+        WebResource webResourceGET = client.resource(url);
         ClientResponse response = webResourceGET.put(ClientResponse.class, pin);
 
         if (response.getStatus() != 200) {
