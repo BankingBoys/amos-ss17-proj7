f3278a3c2f382c1b72f5c7dae5afc78489bd2a91
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-05 11:27:15 +0200

    changed reom fragment to module

--- app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionOverviewFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionOverviewFragment.java
@@ -1,128 +1,93 @@
 package de.fau.amos.virtualledger.android.transactionOverview;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.net.Uri;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.inject.Inject;
 
 import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
+import de.fau.amos.virtualledger.android.api.banking.BankingProvider;
+import de.fau.amos.virtualledger.android.bankingOverview.deleteBankAccessAccount.BankAccessNameExtractor;
+import de.fau.amos.virtualledger.android.bankingOverview.deleteBankAccessAccount.DeleteBankAccessAction;
+import de.fau.amos.virtualledger.android.bankingOverview.deleteBankAccessAccount.LongClickDeleteListenerList;
+import de.fau.amos.virtualledger.android.bankingOverview.deleteBankAccessAccount.functions.BiConsumer;
+import de.fau.amos.virtualledger.android.bankingOverview.expandableList.Adapter.ExpandableAdapterBanking;
+import de.fau.amos.virtualledger.android.bankingOverview.expandableList.Fragment.ExpandableBankFragment;
+import de.fau.amos.virtualledger.android.bankingOverview.expandableList.Fragment.NoBankingAccessesFragment;
+import de.fau.amos.virtualledger.android.bankingOverview.expandableList.model.Group;
+import de.fau.amos.virtualledger.android.bankingOverview.localStorage.BankAccessCredentialDB;
+import de.fau.amos.virtualledger.android.dagger.App;
+import de.fau.amos.virtualledger.dtos.BankAccess;
+import de.fau.amos.virtualledger.dtos.BankAccount;
+import de.fau.amos.virtualledger.dtos.BankAccountSync;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
- * A fragment with a Google +1 button.
- * Activities that contain this fragment must implement the
- * {@link TransactionOverviewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TransactionOverviewFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Created by Simon on 21.05.2017.
  */
+
 public class TransactionOverviewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public TransactionOverviewFragment() {
-        // Required empty public constructor
-    }
+    private static final String TAG = "BankAccessListFragment";
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TransactionOverviewFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static TransactionOverviewFragment newInstance(String param1, String param2) {
-        TransactionOverviewFragment fragment = new TransactionOverviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ExpandableListView listView;
 
+    TextView bankBalanceOverviewText;
+
+    View separator;
+
+    SparseArray<Group> groups = new SparseArray<Group>();
+
+    List<BankAccess> bankAccessList;
+
+    double bankBalanceOverview;
+
+    /**
+     *
+     */
+    @Inject
+    BankingProvider bankingProvider;
+    @Inject
+    AuthenticationProvider authenticationProvider;
+
+
+    /**
+     *
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_transaction_overview, container, false);
-
-        TextView transactionSumText = (TextView) view.findViewById(R.id.transaction_sum_text);
-
-        System.out.println(transactionSumText.getText());//TODO
-        transactionSumText.setText("Hier kommt die Summe hin");//#Baustelle
-        System.out.println(transactionSumText.getText());
-        System.out.println("HHHHHHHHHHHHHHAAAAAAAAAAAAAAALLLLLLLLLLLLLOOOOOOOOOOO");
-
-
-        ListView transactionList = (ListView) view.findViewById(R.id.transaction_list);
-
-        return view;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // Refresh the state of the +1 button each time the activity receives focus.
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     *
      */
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.banking_overview_expandablelist_main_view, container, false);
+        return view;
     }
 
 }
