73f659b90b36785fc627e8ea203af2b71d72671e
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-05 11:14:01 +0200

    mocks added

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/banking/MockedBankingProvider.java
@@ -0,0 +1,148 @@
+package de.fau.amos.virtualledger.android.api.banking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.fau.amos.virtualledger.dtos.BankAccess;
+import de.fau.amos.virtualledger.dtos.BankAccessCredential;
+import de.fau.amos.virtualledger.dtos.BankAccount;
+import de.fau.amos.virtualledger.dtos.BankAccountSync;
+import de.fau.amos.virtualledger.dtos.BankAccountSyncResult;
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+
+/**
+ * Created by sebastian on 14.05.17.
+ */
+
+public class MockedBankingProvider implements BankingProvider {
+
+    public static final int DELAY_TIME_MILLISECUNDS = 300;
+    private String token = "";
+
+
+    @Override
+    public Observable<List<BankAccess>> getBankingOverview() {
+
+        List<BankAccount> accountList = new ArrayList<BankAccount>();
+        BankAccount account = new BankAccount("1.1dummy account id", "1.1 dummy account name1 ", 100);
+        accountList.add(account);
+        account = new BankAccount("1.2dummy account id", "1.2 dummy account name1 ", 100);
+        accountList.add(account);
+
+        BankAccess access = new BankAccess("1dummy access id", " 1 dummy access name ","nochn string","string!!", accountList);
+        access.setBankaccounts(accountList);
+
+        final List<BankAccess> accessList = new ArrayList<BankAccess>();
+        accessList.add(access);
+
+
+        accountList = new ArrayList<BankAccount>();
+        account = new BankAccount("2.1dummy account id", "2.1 dummy account name1 ", 100);
+        accountList.add(account);
+        account = new BankAccount("2.2dummy account id", "2.2 dummy account name1 ", 100);
+        accountList.add(account);
+
+        access = new BankAccess("2dummy access id", " 2 dummy access name ", "nochn string","string!!", accountList);
+        access.setBankaccounts(accountList);
+        accessList.add(access);
+
+
+        final PublishSubject observable = PublishSubject.create();
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //Wait until subject is subscribed
+                    Thread.sleep(DELAY_TIME_MILLISECUNDS);}
+                catch (Exception e){
+
+                }
+                // publish accounts to subject
+                observable.onNext(accessList);
+                observable.onComplete();
+            }
+        });
+        th.start();
+
+        return observable;
+    }
+
+    @Override
+    public Observable<BankAccountSyncResult> getBankingTransactions(List<BankAccountSync> bankAccountSyncList) {
+        return null;
+    }
+
+
+    @Override
+    public Observable<String> addBankAccess(BankAccessCredential bankAccessCredential) {
+        final PublishSubject observable = PublishSubject.create();
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //Wait until subject is subscribed
+                    Thread.sleep(DELAY_TIME_MILLISECUNDS);}
+                catch (Exception e){
+
+                }
+                // publish accounts to subject
+                observable.onNext("BankAccess was added! DummyImplementation...");
+                observable.onComplete();
+            }
+        });
+        th.start();
+
+        return observable;
+    }
+
+    @Override
+    public Observable<String> deleteBankAccess(String accessId) {
+        final PublishSubject observable = PublishSubject.create();
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //Wait until subject is subscribed
+                    Thread.sleep(DELAY_TIME_MILLISECUNDS);}
+                catch (Exception e){
+
+                }
+                // publish accounts to subject
+                observable.onNext("BankAccess was deleted! DummyImplementation...");
+                observable.onComplete();
+            }
+        });
+        th.start();
+
+        return observable;
+    }
+
+    @Override
+    public Observable<String> deleteBankAccount(String accessId, String accountId) {
+        final PublishSubject observable = PublishSubject.create();
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //Wait until subject is subscribed
+                    Thread.sleep(DELAY_TIME_MILLISECUNDS);}
+                catch (Exception e){
+
+                }
+                // publish accounts to subject
+                observable.onNext("BankAccount was deleted! DummyImplementation...");
+                observable.onComplete();
+            }
+        });
+        th.start();
+
+        return observable;
+    }
+
+    @Override
+    public Observable<String> syncBankAccounts(List<BankAccountSync> bankAccountSyncList) {
+        return null;
+    }
+
+}
