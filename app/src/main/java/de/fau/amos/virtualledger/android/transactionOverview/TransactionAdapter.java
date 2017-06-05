bd51fe1ac9dff9b966e9045fa389b9c0e8c78af1
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-05 18:08:51 +0200

    added list view for transaction overview

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionAdapter.java
@@ -0,0 +1,50 @@
+package de.fau.amos.virtualledger.android.transactionOverview;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import de.fau.amos.virtualledger.R;
+
+public class TransactionAdapter extends ArrayAdapter<Transaction> {
+
+
+    public TransactionAdapter(Activity activity, int layout, ArrayList<Transaction> data){
+        super(activity, layout, data);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        System.out.println("++##############Position"+position);
+        Transaction transaction = super.getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.transaction_list_item, parent, false);
+        }
+
+        TextView dateTextView = (TextView) convertView.findViewById(R.id.id_datum);
+        DateFormat formatter = new SimpleDateFormat("dd.MM.YY");
+        dateTextView.setText(formatter.format(transaction.booking().getDate()));
+
+        TextView bankTextView = (TextView) convertView.findViewById(R.id.id_bankname);
+        bankTextView.setText(transaction.bankName());
+
+        TextView amountTextView = (TextView) convertView.findViewById(R.id.id_amount);
+        amountTextView.setText(transaction.booking().getAmount()+"");
+
+        TextView usageTextView = (TextView) convertView.findViewById(R.id.id_usage);
+        usageTextView.setText(transaction.booking().getUsage());
+
+        return convertView;
+    }
+
+
+
+}
--- app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionOverviewFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionOverviewFragment.java
@@ -6,23 +6,25 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import javax.inject.Inject;
 
 import de.fau.amos.virtualledger.R;
 import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
 import de.fau.amos.virtualledger.android.api.banking.BankingProvider;
-
-/**
- * Created by Simon on 21.05.2017.
- */
+import de.fau.amos.virtualledger.dtos.Booking;
 
 public class TransactionOverviewFragment extends Fragment {
     /**
      *
      */
     private TextView sumView = null;
+    private double totalAmount = 0;
 
     /**
      *
@@ -47,11 +49,21 @@ public class TransactionOverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transaction_overview, container, false);
-        this.sumView = (TextView) view.findViewById(R.id.transaction_sum_text);
-        System.out.println(sumView.getText());
-        sumView.setText("Ein Test");
-        System.out.println(sumView.getText());
+        refreshTotalAmount(view);
+
+        ListView bookingListView = (ListView) view.findViewById(R.id.transaction_list);
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        Booking booking = new Booking();
+        booking.setDate(new Date());
+        transactions.add(new Transaction("Testbank", booking));
+        bookingListView.setAdapter(new TransactionAdapter(this.getActivity(), R.id.transaction_list, transactions));
+
         return view;
+    }
+
+    private void refreshTotalAmount(View view) {
+        this.sumView = (TextView) view.findViewById(R.id.transaction_sum_text);
+        sumView.setText("Total amount: " + totalAmount);
     }
 
 }
