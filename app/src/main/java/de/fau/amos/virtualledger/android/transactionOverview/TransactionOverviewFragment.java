df7c13f37a7766a96a4714c31a6b2683e9f822d8
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-05 11:43:51 +0200

    changed inflation

--- app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionOverviewFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/transactionOverview/TransactionOverviewFragment.java
@@ -3,44 +3,26 @@ package de.fau.amos.virtualledger.android.transactionOverview;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
 import android.widget.TextView;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
 import de.fau.amos.virtualledger.R;
 import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
 import de.fau.amos.virtualledger.android.api.banking.BankingProvider;
-import de.fau.amos.virtualledger.android.bankingOverview.expandableList.model.Group;
-import de.fau.amos.virtualledger.dtos.BankAccess;
 
 /**
  * Created by Simon on 21.05.2017.
  */
 
 public class TransactionOverviewFragment extends Fragment {
-    private static final String TAG = "BankAccessListFragment";
-
     /**
      *
      */
-    ExpandableListView listView;
-
-    TextView bankBalanceOverviewText;
-
-    View separator;
-
-    SparseArray<Group> groups = new SparseArray<Group>();
-
-    List<BankAccess> bankAccessList;
-
-    double bankBalanceOverview;
+    private TextView sumView = null;
 
     /**
      *
@@ -64,8 +46,8 @@ public class TransactionOverviewFragment extends Fragment {
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.banking_overview_expandablelist_main_view, container, false);
-        TextView sumView = (TextView) view.findViewById(R.id.transaction_sum_text);
+        View view = inflater.inflate(R.layout.fragment_transaction_overview, container, false);
+        this.sumView = (TextView) view.findViewById(R.id.transaction_sum_text);
         System.out.println(sumView.getText());
         sumView.setText("Ein Test");
         System.out.println(sumView.getText());
