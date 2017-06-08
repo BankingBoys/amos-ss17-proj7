4d0ca1a9860396f4461bcabd5826dd7b87159917
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-08 12:37:18 +0200

    Fixed dagger

--- app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/BankingDataModule.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/BankingDataModule.java
@@ -5,15 +5,16 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import de.fau.amos.virtualledger.android.api.banking.BankingProvider;
+import de.fau.amos.virtualledger.android.bankingOverview.localStorage.BankAccessCredentialDB;
 import de.fau.amos.virtualledger.android.data.BankingDataManager;
 
-@Module(includes = {BankingModule.class})
+@Module(includes = {BankingModule.class, DatabaseModule.class})
 public class BankingDataModule {
 
 
     @Provides
     @Singleton
-    BankingDataManager provideBankingDataManager(BankingProvider bankingProvider) {
+    BankingDataManager provideBankingDataManager(BankingProvider bankingProvider, BankAccessCredentialDB bankAccessCredentialDB) {
         return new BankingDataManager(bankingProvider, bankAccessCredentialDB);
     }
 }
