0d65dbea624be28187aaf5abedb6a3ac051eabb7
Georg <georg.schwarz@fau.de>
2017-06-10 20:50:03 +0200

    - added calendar to CalendarViewFragment (Caldroid) - had to do a workaround because Caldroid uses support.v4.app.Fragment

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalendarViewFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalendarViewFragment.java
@@ -1,13 +1,21 @@
 package de.fau.amos.virtualledger.android.views.calendar;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.LinearLayout;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,7 +26,14 @@ public class CalendarViewFragment extends Fragment {
     private static final String TAG = CalendarViewFragment.class.getSimpleName();
 
     @BindView(R.id.calendar_view_fragment_calendar_wrapper)
-    CalendarView calendarView;
+    LinearLayout calendarWrapper;
+
+    // need FragmentActivity because of Caldroid workaround
+    private FragmentActivity context;
+
+
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -36,5 +51,23 @@ public class CalendarViewFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH));
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction transaction = context.getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.calendar_view_fragment_calendar_wrapper, caldroidFragment, "calendar_view_fragment_calendar");
+        transaction.commit();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        // workaround because we need getSupportFragmentManager() for Caldroid
+        this.context = (FragmentActivity) context;
+        super.onAttach(context);
     }
 }
