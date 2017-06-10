bda9ebeece7e5f245b13b7299b973928be0f64a1
Georg <georg.schwarz@fau.de>
2017-06-10 18:55:18 +0200

    - created fragment for calendar view

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderViewFragment.java
@@ -0,0 +1,31 @@
+package de.fau.amos.virtualledger.android.views.calendar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.dagger.App;
+import de.fau.amos.virtualledger.android.views.transactionOverview.TransactionOverviewFragment;
+
+public class CalenderViewFragment extends Fragment {
+    private static final String TAG = CalenderViewFragment.class.getSimpleName();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ((App) getActivity().getApplication()).getNetComponent().inject(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_transaction_overview, container, false);
+        return view;
+    }
+}
