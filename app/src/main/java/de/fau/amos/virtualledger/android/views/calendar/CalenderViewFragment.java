ba525c4bc41aa0be0c6ddd41acd0eae0696f48d7
Georg <georg.schwarz@fau.de>
2017-06-10 18:58:13 +0200

    - added calendar_view_fragment.xml and load it in CalenderViewFragment.java

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderViewFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderViewFragment.java
@@ -25,7 +25,7 @@ public class CalenderViewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_transaction_overview, container, false);
+        View view = inflater.inflate(R.layout.calendar_view_fragment, container, false);
         return view;
     }
 }
