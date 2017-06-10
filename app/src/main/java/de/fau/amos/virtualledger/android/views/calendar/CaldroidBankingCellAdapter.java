fab78c749260f80c355a5eb6e41bd2729bfbefd6
Georg <georg.schwarz@fau.de>
2017-06-10 21:43:27 +0200

    - added custom cell design and copied implementation of getView in CaldroidBankingCellAdapter from github

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CaldroidBankingCellAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CaldroidBankingCellAdapter.java
@@ -1,17 +1,20 @@
 package de.fau.amos.virtualledger.android.views.calendar;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidGridAdapter;
-import com.roomorama.caldroid.CellView;
 
 import java.util.Map;
 
 import de.fau.amos.virtualledger.R;
+import hirondelle.date4j.DateTime;
 
 /**
  * Created by Georg on 10.06.2017.
@@ -31,21 +34,91 @@ public class CaldroidBankingCellAdapter extends CaldroidGridAdapter {
         super(context, month, year, caldroidData, extraData);
     }
 
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        TextView cellView = (TextView) convertView;
+        View cellView = convertView;
 
+        // For reuse
         if (convertView == null) {
-            if (squareTextViewCell) {
-                cellView = (TextView) inflater.inflate(R.layout.square_date_cell, null);
+            cellView = inflater.inflate(R.layout.calendar_view_cell, null);
+        }
+
+        int topPadding = cellView.getPaddingTop();
+        int leftPadding = cellView.getPaddingLeft();
+        int bottomPadding = cellView.getPaddingBottom();
+        int rightPadding = cellView.getPaddingRight();
+
+        TextView dateTextView = (TextView) cellView.findViewById(R.id.calendar_view_cell_date);
+        TextView amountTextView = (TextView) cellView.findViewById(R.id.calendar_view_cell_amount);
+
+        dateTextView.setTextColor(Color.BLACK);
+
+        // Get dateTime of this cell
+        DateTime dateTime = this.datetimeList.get(position);
+        Resources resources = context.getResources();
+
+        // Set color of the dates in previous / next month
+        if (dateTime.getMonth() != month) {
+            dateTextView.setTextColor(resources
+                    .getColor(com.caldroid.R.color.caldroid_darker_gray));
+        }
+
+        boolean shouldResetDiabledView = false;
+        boolean shouldResetSelectedView = false;
+
+        // Customize for disabled dates and date outside min/max dates
+        if ((minDateTime != null && dateTime.lt(minDateTime))
+                || (maxDateTime != null && dateTime.gt(maxDateTime))
+                || (disableDates != null && disableDates.indexOf(dateTime) != -1)) {
+
+            dateTextView.setTextColor(CaldroidFragment.disabledTextColor);
+            if (CaldroidFragment.disabledBackgroundDrawable == -1) {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.disable_cell);
             } else {
-                cellView = (TextView) inflater.inflate(R.layout.normal_date_cell, null);
+                cellView.setBackgroundResource(CaldroidFragment.disabledBackgroundDrawable);
+            }
+
+            if (dateTime.equals(getToday())) {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.red_border_gray_bg);
+            }
+
+        } else {
+            shouldResetDiabledView = true;
+        }
+
+        // Customize for selected dates
+        if (selectedDates != null && selectedDates.indexOf(dateTime) != -1) {
+            cellView.setBackgroundColor(resources
+                    .getColor(com.caldroid.R.color.caldroid_sky_blue));
+
+            dateTextView.setTextColor(Color.BLACK);
+
+        } else {
+            shouldResetSelectedView = true;
+        }
+
+        if (shouldResetDiabledView && shouldResetSelectedView) {
+            // Customize for today
+            if (dateTime.equals(getToday())) {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.red_border);
+            } else {
+                cellView.setBackgroundResource(com.caldroid.R.drawable.cell_bg);
             }
         }
 
-        customizeTextView(position, (CellView) cellView);
+        dateTextView.setText("" + dateTime.getDay());
+        amountTextView.setText("2500");
+
+        // Somehow after setBackgroundResource, the padding collapse.
+        // This is to recover the padding
+        //cellView.setPadding(leftPadding, topPadding, rightPadding, bottomPadding);
+        cellView.setPadding(3, 2, 2, 3);
+
+        // Set custom color if required
+        setCustomResources(dateTime, cellView, dateTextView);
 
         return cellView;
     }
