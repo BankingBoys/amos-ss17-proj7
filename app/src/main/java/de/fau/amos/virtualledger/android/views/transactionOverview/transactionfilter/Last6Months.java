bdd77736bfba610e18eec9bff906253ffed3ffb6
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-11 16:11:31 +0200

    added last 6 months transaction filter

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/transactionOverview/transactionfilter/Last6Months.java
@@ -0,0 +1,15 @@
+package de.fau.amos.virtualledger.android.views.transactionOverview.transactionfilter;
+
+import de.fau.amos.virtualledger.android.views.transactionOverview.Transaction;
+import de.fau.amos.virtualledger.android.views.transactionOverview.TransactionFilter;
+
+/**
+ * Created by sebastian on 11.06.17.
+ */
+
+public class Last6Months implements TransactionFilter{
+    @Override
+    public boolean shouldBeRemoved(Transaction t) {
+        return new LastNMonths(6).shouldBeRemoved(t);
+    }
+}
