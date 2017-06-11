ec1c652d202ad66054e020a9d28122620f9bd431
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-11 15:12:55 +0200

    added automated slack message when server is up or down

---
+++ tools/post_slack_server_status.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Usage: slackpost <token> <channel> <message>
+
+# Enter the name of your slack host here - the thing that appears in your URL:
+# https://slackhost.slack.com/
+server="13.58.27.176"
+text=""
+if nc -w 5 -z "$server" 22 ; then
+    echo "Port 22 on $server is open"
+    text="serverUp"
+else
+    text="Serverdeploy doenst work!!!! Commit{"$TRAVIS_COMMIT_MESSAGE"}"
+fi
+
+token="WF2UyrAWwmuoSgjjtqPo8O1N"
+channel="#server"
+escapedText=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
+json="{\"text\": \"$escapedText\"}"
+
+curl -s -d "payload=$json" $slackwebhook
