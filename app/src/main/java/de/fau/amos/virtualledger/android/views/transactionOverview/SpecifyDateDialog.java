4d3fb92eec1e8f67aea0d714e66cec5cc7901714
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-12 22:52:32 +0200

    added onKlickListener to specify date dialog wich opens a date chooser and writes back the chosen date

--- app/src/main/java/de/fau/amos/virtualledger/android/views/transactionOverview/SpecifyDateDialog.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/transactionOverview/SpecifyDateDialog.java
@@ -1,19 +1,65 @@
 package de.fau.amos.virtualledger.android.views.transactionOverview;
 
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.logging.Logger;
 
 import de.fau.amos.virtualledger.R;
 
 public class SpecifyDateDialog extends DialogFragment {
 
-    /**
-     * Create a new instance of MyDialogFragment, providing "num"
-     * as an argument.
-     */
+    private Calendar startDate = new GregorianCalendar();
+    private Calendar endDate = new GregorianCalendar();
+
+
+    DatePickerDialog.OnDateSetListener startDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                      int dayOfMonth) {
+                    // TODO Auto-generated method stub
+                    startDate.set(Calendar.YEAR, year);
+                    startDate.set(Calendar.MONTH, monthOfYear);
+                    startDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    logger().info("Startdate changed to: " + startDate);
+                }
+
+                private Logger logger() {
+                    return Logger.getLogger(this.getClass().getCanonicalName() + "{" + this.hashCode() + "}");
+                }
+
+            };
+
+    DatePickerDialog.OnDateSetListener endDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                      int dayOfMonth) {
+                    // TODO Auto-generated method stub
+                    endDate.set(Calendar.YEAR, year);
+                    endDate.set(Calendar.MONTH, monthOfYear);
+                    endDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    logger().info("Startdate changed to: " + endDate);
+                }
+
+                private Logger logger() {
+                    return Logger.getLogger(this.getClass().getCanonicalName() + "{" + this.hashCode() + "}");
+                }
+
+            };
+
+
     static SpecifyDateDialog newInstance() {
         return new SpecifyDateDialog();
     }
@@ -27,6 +73,25 @@ public class SpecifyDateDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.date_range_chooser_dialog, container, false);
+        Button startDateButton = (Button) v.findViewById(R.id.start_date_button);
+        startDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(v.getContext(), startDateListener, startDate
+                        .get(Calendar.YEAR), startDate.get(Calendar.MONTH),
+                        startDate.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        final Button endDateButton = (Button) v.findViewById(R.id.end_date_button);
+        endDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(v.getContext(), endDateListener, endDate
+                        .get(Calendar.YEAR), endDate.get(Calendar.MONTH),
+                        endDate.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
         return v;
     }
 }
