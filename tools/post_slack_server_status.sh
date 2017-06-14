3ef21041d55e88915f43f717ca5a4509c47d7f63
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-14 15:53:05 +0200

    enhanced slack-canary. Added hockeyAppDeployStatus and ServerDeployStatus

--- tools/post_slack_server_status.sh
+++ tools/post_slack_server_status.sh
@@ -6,12 +6,29 @@
 # https://slackhost.slack.com/
 server="13.58.27.176"
 text=""
+
+# HockeyAppDeploy
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+	text=$text"HockeyAppDeploy{FALSE}"
+else 
+	text=$text"HockeyAppDeploy{TRUE}"
+fi
+
+# Amazon AWS Deploy
+if [ "$DEPLOY_SERVER" != "TRUE" ]; then
+	text=$text", ServerDeploy:{TRUE}"
+else
+	text=$text", ServerDeploy:{FALSE}"
+fi
+
+# Amazon Server Status
 if nc -w 5 -z "$server" 8080 ; then
     echo "Port 22 on $server is open"
-    text="serverUp"
+    text=$text", ServerStatus:{UP}"
 else
-    text="Serverdeploy doenst work!!!! Commit{"$TRAVIS_COMMIT_MESSAGE"}"
+    text="!!!ATTENTION!!!::: "$text", ServerStatus:{DOWN}Commit{"$TRAVIS_COMMIT_MESSAGE"}"
 fi
+
 
 token="WF2UyrAWwmuoSgjjtqPo8O1N"
 channel="#server"
