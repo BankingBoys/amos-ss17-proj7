521ae8cb8282d91833cce0185df8857e6d0d500a
Georg <georg.schwarz@fau.de>
2017-06-15 10:52:41 +0200

    transaction implements interface Parcelable in order to pass it to fragments via Bundle

--- app/src/main/java/de/fau/amos/virtualledger/android/views/transactionOverview/Transaction.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/transactionOverview/Transaction.java
@@ -1,12 +1,17 @@
 package de.fau.amos.virtualledger.android.views.transactionOverview;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+
 import de.fau.amos.virtualledger.dtos.Booking;
 
 /**
  * Created by sebastian on 05.06.17.
  */
 
-public class Transaction {
+public class Transaction implements Parcelable {
 
     private String bankName;
     private Booking booking;
@@ -23,5 +28,40 @@ public class Transaction {
 
     public String bankName(){
         return this.bankName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(bankName);
+        dest.writeLong(booking.getDate().getTime());
+        dest.writeDouble(booking.getAmount());
+        dest.writeString(booking.getUsage());
+    }
+
+
+    public static final Creator<Transaction> CREATOR = new Creator<Transaction>() {
+        @Override
+        public Transaction createFromParcel(Parcel in) {
+            return new Transaction(in);
+        }
+
+        @Override
+        public Transaction[] newArray(int size) {
+            return new Transaction[size];
+        }
+    };
+
+    protected Transaction(Parcel in) {
+        bankName = in.readString();
+        Date date = new Date(in.readLong());
+        double amount = in.readDouble();
+        String usage = in.readString();
+        booking = new Booking(date, amount);
+        booking.setUsage(usage);
     }
 }
