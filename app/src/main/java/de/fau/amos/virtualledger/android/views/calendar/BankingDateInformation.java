1f294d52faf281d33f05c4fb8c5ba0f637737f6b
Georg <georg.schwarz@fau.de>
2017-06-15 09:56:47 +0200

    added list with  bookings to BankingDateInformation

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/BankingDateInformation.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/BankingDateInformation.java
@@ -1,5 +1,9 @@
 package de.fau.amos.virtualledger.android.views.calendar;
 
+import java.util.List;
+
+import de.fau.amos.virtualledger.dtos.Booking;
+
 /**
  * Created by Georg on 15.06.2017.
  */
@@ -8,10 +12,12 @@ public class BankingDateInformation {
 
     private double amount;
     private double amountDelta;
+    private List<Booking> bookingList;
 
-    public BankingDateInformation(double amount, double amountDelta) {
+    public BankingDateInformation(double amount, double amountDelta, List<Booking> bookingList) {
         this.amount = amount;
         this.amountDelta = amountDelta;
+        this.bookingList = bookingList;
     }
 
     public double getAmount() {
@@ -20,5 +26,9 @@ public class BankingDateInformation {
 
     public double getAmountDelta() {
         return amountDelta;
+    }
+
+    public List<Booking> getBookingList() {
+        return bookingList;
     }
 }
