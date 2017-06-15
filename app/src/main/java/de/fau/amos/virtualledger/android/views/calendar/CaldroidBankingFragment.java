ded80996cc00b92ddce12adc55292ee3bd3b16e9
Georg <georg.schwarz@fau.de>
2017-06-15 10:03:43 +0200

    CaldroidBankingFragment contains Map that is required for CaldroidGridAdapter as member and initializes

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CaldroidBankingFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CaldroidBankingFragment.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidGridAdapter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,7 +14,9 @@ import javax.inject.Inject;
 import de.fau.amos.virtualledger.android.dagger.App;
 import de.fau.amos.virtualledger.android.data.BankingDataManager;
 import de.fau.amos.virtualledger.android.data.BankingSyncFailedException;
+import de.fau.amos.virtualledger.android.views.transactionOverview.TransactionsComparator;
 import de.fau.amos.virtualledger.dtos.BankAccountBookings;
+import de.fau.amos.virtualledger.dtos.Booking;
 import hirondelle.date4j.DateTime;
 
 /**
@@ -25,26 +28,38 @@ public class CaldroidBankingFragment extends CaldroidFragment {
     @Inject
     BankingDataManager bankingDataManager;
 
-    private List<BankAccountBookings> bankAccountBookingsList;
+    private HashMap<DateTime, BankingDateInformation> bankingDateInformationMap;
 
     @Override
     public CaldroidGridAdapter getNewDatesGridAdapter(int month, int year) {
         ((App) getActivity().getApplication()).getNetComponent().inject(this);
         init();
 
-        HashMap<DateTime, BankingDateInformation> bankingDateInformationMap = new HashMap<>();
-
         return new CaldroidBankingCellAdapter(getContext(), month, year, getCaldroidData(), getExtraData(), bankingDateInformationMap );
     }
 
     private void init() {
+        bankingDateInformationMap = new HashMap<>();
+        List<BankAccountBookings> bankAccountBookingsList = new ArrayList<>();
+
         try {
             bankAccountBookingsList = bankingDataManager.getBankAccountBookings();
-            if ((bankAccountBookingsList == null || bankAccountBookingsList.size() == 0)) {
-                //do nothing
-            }
         } catch (BankingSyncFailedException ex) {
             Toast.makeText(getActivity(), "Failed connecting to the server, try again later", Toast.LENGTH_LONG).show();
+        }
+
+        // TODO order List
+
+        for(int i = 0; i < bankAccountBookingsList.size(); ++i) {
+            BankAccountBookings bankAccountBookings = bankAccountBookingsList.get(i);
+
+            for(Booking booking : bankAccountBookings.getBookings()) {
+
+            }
+            double amount = 0.0;
+            double amountDelta = 0.0;
+
+            BankingDateInformation bankingDateInformation = new BankingDateInformation(amount, amountDelta, bankAccountBookings.getBookings());
         }
     }
 }
