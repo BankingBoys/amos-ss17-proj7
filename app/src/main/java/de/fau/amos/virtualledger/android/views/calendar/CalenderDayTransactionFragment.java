564525335e7d76bd9f0730cf9459f1a48f9be618
simonschmoll <simon.schmoll@gmail.com>
2017-06-18 20:16:08 +0200

    layout is now opened in Fragment

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
@@ -1,10 +1,37 @@
 package de.fau.amos.virtualledger.android.views.calendar;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import de.fau.amos.virtualledger.R;
 
 /**
  * Created by Simon on 18.06.2017.
  */
 
 public class CalenderDayTransactionFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.calendar_view_daily_transactions, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
 }
