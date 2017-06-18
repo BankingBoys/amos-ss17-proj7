15d65dc13f5273abfa60d6acf42430024baf78f7
simonschmoll <simon.schmoll@gmail.com>
2017-06-18 20:56:32 +0200

    added new method for initializing the DailyTransaction Fragment

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
@@ -1,6 +1,8 @@
 package de.fau.amos.virtualledger.android.views.calendar;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,6 +11,8 @@ import android.view.ViewGroup;
 
 import butterknife.BindView;
 import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.views.shared.totalAmount.TotalAmountFragment;
+import de.fau.amos.virtualledger.android.views.shared.transactionList.BankTransactionSupplier;
 
 /**
  * Created by Simon on 18.06.2017.
@@ -16,9 +20,12 @@ import de.fau.amos.virtualledger.R;
 
 public class CalenderDayTransactionFragment extends Fragment {
 
+    private static final String BUNDLE_PARAMETER_TOTALAMOUNT = "totalamount";
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
 
     }
 
@@ -29,9 +36,19 @@ public class CalenderDayTransactionFragment extends Fragment {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+    public static CalenderDayTransactionFragment newInstance(double totalAmount) {
+        Bundle bundle = new Bundle();
+        bundle.putDouble(BUNDLE_PARAMETER_TOTALAMOUNT, totalAmount);
+        CalenderDayTransactionFragment fragment = new CalenderDayTransactionFragment();
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 
 }
