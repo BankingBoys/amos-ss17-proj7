8c3a97900de14a3e8aa81d04d1736e3168d47e62
Georg <georg.schwarz@fau.de>
2017-06-18 10:15:47 +0200

    added activity empty for choosing which login is preferred

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/authentication/AuthenticationChooserActivity.java
@@ -0,0 +1,22 @@
+package de.fau.amos.virtualledger.android.authentication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.fau.amos.virtualledger.R;
+
+/**
+ * Created by Georg on 18.06.2017.
+ */
+
+public class AuthenticationChooserActivity extends Activity {
+    private static final String TAG = AuthenticationChooserActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setContentView(R.layout.authentication_chooser);
+    }
+}
