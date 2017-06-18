edd92c34a1cd7b963b6e308ade44bef704590036
simonschmoll <simon.schmoll@gmail.com>
2017-06-18 19:26:54 +0200

    added new Fragment for showing a transaction screen after clicking on a day in the calender

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
@@ -0,0 +1,10 @@
+package de.fau.amos.virtualledger.android.views.calendar;
+
+import android.app.Fragment;
+
+/**
+ * Created by Simon on 18.06.2017.
+ */
+
+public class CalenderDayTransactionFragment extends Fragment {
+}
