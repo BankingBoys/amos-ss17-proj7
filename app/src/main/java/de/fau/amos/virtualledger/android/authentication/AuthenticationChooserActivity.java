443e2462f809a06ff6cb56dd86fbf126c4b596ca
Georg <georg.schwarz@fau.de>
2017-06-18 10:18:57 +0200

    setup basics for AuthenticationChooserActivity (butterknife and bind button events)

--- app/src/main/java/de/fau/amos/virtualledger/android/authentication/AuthenticationChooserActivity.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/authentication/AuthenticationChooserActivity.java
@@ -2,7 +2,11 @@ package de.fau.amos.virtualledger.android.authentication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.fau.amos.virtualledger.R;
 
 /**
@@ -16,7 +20,19 @@ public class AuthenticationChooserActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
 
-        // setContentView(R.layout.authentication_chooser);
+        setContentView(R.layout.authentication_activity_chooser);
+    }
+
+
+    @OnClick(R.id.authentication_activity_chooser_demo_workflow_button)
+    public void onInitDemoWorkflow() {
+
+    }
+
+    @OnClick(R.id.authentication_activity_chooser_oidc_workflow_button)
+    public void onInitOidcWorkflow() {
+
     }
 }
