0053b094cd826f4a5a8a46507a5085d768f05675
simonschmoll <simon.schmoll@gmail.com>
2017-06-20 09:32:42 +0200

    optimized imports

--- app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/calendar/CalenderDayTransactionFragment.java
@@ -1,8 +1,6 @@
 package de.fau.amos.virtualledger.android.views.calendar;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
