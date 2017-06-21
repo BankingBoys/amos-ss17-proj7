23f5562eb753393b48418f7dce0d93120ce4b033
Georg <georg.schwarz@fau.de>
2017-06-21 10:51:01 +0200

    added test for creating list in BankAccountFactory

--- server/src/test/java/de/fau/amos/virtualledger/server/factories/BankAccountFactoryTest.java
+++ server/src/test/java/de/fau/amos/virtualledger/server/factories/BankAccountFactoryTest.java
@@ -7,6 +7,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Georg on 21.06.2017.
  */
@@ -41,5 +44,35 @@ public class BankAccountFactoryTest {
         Assert.assertEquals(accountId, bankAccount.getBankid());
         Assert.assertEquals(type, bankAccount.getName());
         Assert.assertEquals(amount, bankAccount.getBalance(), 0.01);
+    }
+
+    @Test
+    public void createList_success() {
+        // SETUP
+        double amount = 123.22;
+        BankAccountBalanceBankingModel bankAccountBalanceBankingModel = new BankAccountBalanceBankingModel();
+        bankAccountBalanceBankingModel.setReadyHbciBalance(amount);
+
+        String accessId = "accessID";
+        String accountId = "accountId";
+        String type = "type";
+        BankAccountBankingModel bankAccountBankingModel = new BankAccountBankingModel();
+        bankAccountBankingModel.setBankAccessId(accessId);
+        bankAccountBankingModel.setId(accountId);
+        bankAccountBankingModel.setTypeHbciAccount(type);
+        bankAccountBankingModel.setBankAccountBalance(bankAccountBalanceBankingModel);
+
+        List<BankAccountBankingModel> bankAccountBankingModelList = new ArrayList<>();
+        bankAccountBankingModelList.add(bankAccountBankingModel);
+
+        // ACT
+        List<BankAccount> bankAccounts = bankAccountFactory.createBankAccounts(bankAccountBankingModelList);
+
+        // ASSERT
+        Assert.assertNotNull(bankAccounts);
+        Assert.assertEquals(bankAccounts.size(), 1);
+        Assert.assertEquals(accountId, bankAccounts.get(0).getBankid());
+        Assert.assertEquals(type, bankAccounts.get(0).getName());
+        Assert.assertEquals(amount, bankAccounts.get(0).getBalance(), 0.01);
     }
 }
