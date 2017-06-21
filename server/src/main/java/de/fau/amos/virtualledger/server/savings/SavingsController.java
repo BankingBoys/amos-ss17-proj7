ab99665b87d2992708304623d98a316adc941b8d
Georg <georg.schwarz@fau.de>
2017-06-21 18:44:53 +0200

    created class SavingsController with dummy-wise implementation

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/savings/SavingsController.java
@@ -0,0 +1,26 @@
+package de.fau.amos.virtualledger.server.savings;
+
+import de.fau.amos.virtualledger.dtos.SavingsAccount;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+public class SavingsController {
+
+
+    List<SavingsAccount> savingsAccountList = new ArrayList<>();
+
+    public List<SavingsAccount> getSavingAccounts(String email) {
+
+        return savingsAccountList;
+    }
+
+    private void addSavingAccount(String email, SavingsAccount savingsAccount) {
+        
+        savingsAccountList.add(savingsAccount);
+    }
+}
+
