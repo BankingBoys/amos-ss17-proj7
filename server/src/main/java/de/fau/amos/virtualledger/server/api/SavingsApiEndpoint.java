5e71cecca64ba0a21c0b23ea2832b8c5e77f2146
Georg <georg.schwarz@fau.de>
2017-06-21 18:35:25 +0200

    added empty add savings account endpoint

--- server/src/main/java/de/fau/amos/virtualledger/server/api/SavingsApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/SavingsApiEndpoint.java
@@ -5,9 +5,7 @@ import de.fau.amos.virtualledger.server.auth.Secured;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -42,6 +40,40 @@ public class SavingsApiEndpoint {
         return this.getSavingAccounts(email);
     }
 
+
+    /**
+     * Adds a saving accounts to the authenticated user
+     * @param securityContext
+     * @param savingsAccount
+     * @return status 201 if successful
+     */
+    @POST
+    @Secured
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addSavingAccountEndpoint(@Context SecurityContext securityContext, SavingsAccount savingsAccount) {
+        if(securityContext.getUserPrincipal().getName() == null || securityContext.getUserPrincipal().getName().isEmpty())
+        {
+            return Response.status(Response.Status.FORBIDDEN).entity("Authentication failed! Your email wasn't found.").build();
+        }
+        final String email = securityContext.getUserPrincipal().getName();
+        logger.info("getSavingAccountsEndpoint of " + email + " was requested");
+
+        return this.addSavingAccount(email, savingsAccount);
+    }
+
+    /**
+     * Does the logic for adding a saving account to a specific user.
+     * Handles exceptions and returns corresponding response codes.
+     * @param email
+     * @param savingsAccount
+     * @return status 201 if successful
+     */
+    private Response addSavingAccount(String email, SavingsAccount savingsAccount) {
+        // TODO implement
+        return Response.status(Response.Status.CREATED).build();
+    }
+
     /**
      * Does the logic for getting all saving accounts to a specific user.
      * Handles exceptions and returns corresponding response codes.
@@ -49,6 +81,7 @@ public class SavingsApiEndpoint {
      * @return
      */
     private Response getSavingAccounts(String email) {
+        // TODO implement a SavingsController and call it, do exception handling
         List<SavingsAccount> savingsAccountList = new ArrayList<>();
         for(int i = 0; i < 5; ++i) {
             SavingsAccount savingsAccount = new SavingsAccount("id1", "savingsaccount1", 500.00, 123.12, new Date());
