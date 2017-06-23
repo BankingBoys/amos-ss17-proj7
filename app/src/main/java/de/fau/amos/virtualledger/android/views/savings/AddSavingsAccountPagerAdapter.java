6128950efcf7a24c5da6d5da36f9650035c18889
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-24 01:26:54 +0200

    Layout for enter name fragment

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/AddSavingsAccountPagerAdapter.java
@@ -0,0 +1,23 @@
+package de.fau.amos.virtualledger.android.views.savings;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class AddSavingsAccountPagerAdapter extends FragmentPagerAdapter {
+
+    public AddSavingsAccountPagerAdapter(final FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(final int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
