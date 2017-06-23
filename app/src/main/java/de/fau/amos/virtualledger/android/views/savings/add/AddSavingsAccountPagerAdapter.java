7018e3eb0045ed4dc9659eeeda3cc2666db1b4cc
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-24 01:47:23 +0200

    Add second fragment to AddSavingsAccountPagerAdapter

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountPagerAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountPagerAdapter.java
@@ -3,9 +3,12 @@ package de.fau.amos.virtualledger.android.views.savings.add;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 
 public class AddSavingsAccountPagerAdapter extends FragmentPagerAdapter {
+    @SuppressWarnings("unused")
+    private static final String TAG = AddSavingsAccountPagerAdapter.class.getSimpleName();
 
     public AddSavingsAccountPagerAdapter(final FragmentManager fm) {
         super(fm);
@@ -13,11 +16,19 @@ public class AddSavingsAccountPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(final int position) {
-        return new AddSavingsAccountNameFragment();
+        switch (position) {
+            case 0:
+                return new AddSavingsAccountNameFragment();
+            case 1:
+                return new AddSavingsAccountAmountFragment();
+            default:
+                Log.e(TAG, "Trying to switch to invalid pager position");
+                return null;
+        }
     }
 
     @Override
     public int getCount() {
-        return 1;
+        return 2;
     }
 }
