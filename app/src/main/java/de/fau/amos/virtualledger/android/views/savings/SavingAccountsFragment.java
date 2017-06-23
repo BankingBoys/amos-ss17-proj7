a0fcf9ff2c4c8c3e9476dbd2c0c0633c82e0a73b
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-23 17:14:30 +0200

    Add Button to open AddSavingsAccountActivity

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingAccountsFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingAccountsFragment.java
@@ -1,9 +1,13 @@
 package de.fau.amos.virtualledger.android.views.savings;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,7 +26,26 @@ public class SavingAccountsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
         return inflater.inflate(R.layout.saving_accounts_list, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
+        inflater.inflate(R.menu.saving_accounts_app_bar, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.saving_accounts_app_bar_add:
+                final Intent addSavingsAccountIntent = new Intent(getActivity(), AddSavingsAccountActivity.class);
+                startActivity(addSavingsAccountIntent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
