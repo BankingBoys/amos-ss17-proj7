b705e0d6396f1bd67b1b8577ba9480068963677f
Georg <georg.schwarz@fau.de>
2017-06-23 17:10:45 +0200

    use the SavingsAccountRepository instead of storing in RAM in SavingsController

--- server/src/main/java/de/fau/amos/virtualledger/server/savings/SavingsController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/savings/SavingsController.java
@@ -1,26 +1,31 @@
 package de.fau.amos.virtualledger.server.savings;
 
 import de.fau.amos.virtualledger.server.model.SavingsAccount;
+import de.fau.amos.virtualledger.server.persistence.SavingsAccountRepository;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.ArrayList;
+import javax.inject.Inject;
 import java.util.List;
 
 @ApplicationScoped
 public class SavingsController {
 
+    private SavingsAccountRepository savingsAccountRepository;
 
-    List<SavingsAccount> savingsAccountList = new ArrayList<>();
+    @Inject
+    public SavingsController(SavingsAccountRepository savingsAccountRepository) {
+        this.savingsAccountRepository = savingsAccountRepository;
+    }
+    protected  SavingsController () { }
 
     public List<SavingsAccount> getSavingAccounts(String email) {
 
-        return savingsAccountList;
+        return savingsAccountRepository.getSavingsAccountsByUserEmail(email);
     }
 
     public void addSavingAccount(String email, SavingsAccount savingsAccount) throws SavingsException {
 
-        savingsAccount.id = savingsAccountList.size();
-        savingsAccountList.add(savingsAccount);
+        savingsAccountRepository.createSavingsAccount(email, savingsAccount);
     }
 }
 
