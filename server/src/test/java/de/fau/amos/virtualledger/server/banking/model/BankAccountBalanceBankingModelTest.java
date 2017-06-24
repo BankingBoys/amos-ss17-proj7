de471505e7ff053fed78efa50bcbc1213a0dec92
simonschmoll <simon.schmoll@gmail.com>
2017-06-24 14:24:31 +0200

    added test for the constructor

--- server/src/test/java/de/fau/amos/virtualledger/server/banking/model/BankAccountBalanceBankingModelTest.java
+++ server/src/test/java/de/fau/amos/virtualledger/server/banking/model/BankAccountBalanceBankingModelTest.java
@@ -1,8 +1,17 @@
 package de.fau.amos.virtualledger.server.banking.model;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 /**
  * Created by Simon on 24.06.2017.
  */
 public class BankAccountBalanceBankingModelTest {
+
+    @Test
+    public void constructorTest() {
+        BankAccountBalanceBankingModel testModel = new BankAccountBalanceBankingModel();
+        Assertions.assertThat(testModel).isNotNull();
+    }
     
 }
