fb2620b8b29c10ad793be6f643d2a51c70857f1b
simonschmoll <simon.schmoll@gmail.com>
2017-06-24 15:05:42 +0200

    added test for setting and getting testAccessId

--- server/src/test/java/de/fau/amos/virtualledger/server/banking/model/BankAccountBankingModelTest.java
+++ server/src/test/java/de/fau/amos/virtualledger/server/banking/model/BankAccountBankingModelTest.java
@@ -1,6 +1,8 @@
 package de.fau.amos.virtualledger.server.banking.model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by Simon on 24.06.2017.
@@ -12,5 +14,12 @@ public class BankAccountBankingModelTest {
     @Before
     public void setUp() {
         testModel = new BankAccountBankingModel();
+    }
+
+    @Test
+    public void setAndGetBankAccessId() {
+        String testString = "testId";
+        testModel.setBankAccessId(testString);
+        Assertions.assertThat(testModel.getBankAccessId()).isEqualTo(testString);
     }
 }
