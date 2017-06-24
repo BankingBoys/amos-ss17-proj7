8db014b00c8d69ef2b4616f21ab367fd5e78ee60
simonschmoll <simon.schmoll@gmail.com>
2017-06-24 15:02:04 +0200

    added setUp method for test class so that the tested class will  be initialized before the main test classes

--- server/src/test/java/de/fau/amos/virtualledger/server/banking/model/BankAccountBankingModelTest.java
+++ server/src/test/java/de/fau/amos/virtualledger/server/banking/model/BankAccountBankingModelTest.java
@@ -1,7 +1,16 @@
 package de.fau.amos.virtualledger.server.banking.model;
 
+import org.junit.Before;
+
 /**
  * Created by Simon on 24.06.2017.
  */
 public class BankAccountBankingModelTest {
+
+    BankAccountBankingModel testModel;
+
+    @Before
+    public void setUp() {
+        testModel = new BankAccountBankingModel();
+    }
 }
