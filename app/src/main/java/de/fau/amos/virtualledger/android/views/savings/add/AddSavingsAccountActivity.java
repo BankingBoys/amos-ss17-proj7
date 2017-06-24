2f10a137a1f845febac1e14efe2bd0fa77a3cf92
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-24 02:13:14 +0200

    Do not show previous button on first page

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountActivity.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountActivity.java
@@ -3,6 +3,7 @@ package de.fau.amos.virtualledger.android.views.savings.add;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
@@ -18,6 +19,9 @@ public class AddSavingsAccountActivity extends AppCompatActivity {
 
     @BindView(R.id.add_savings_account_pager)
     ViewPager viewPager;
+
+    @BindView(R.id.add_savings_account_button_previous)
+    Button buttonPrevious;
 
     @BindView(R.id.add_savings_account_button_next)
     Button buttonNext;
@@ -55,8 +59,11 @@ public class AddSavingsAccountActivity extends AppCompatActivity {
 
     @OnPageChange(R.id.add_savings_account_pager)
     void onPagerPageChange(final int position) {
-        buttonNext.setText(position == pagerAdapter.getCount() - 1
+        buttonNext.setText(position >= pagerAdapter.getCount() - 1
                 ? R.string.add_savings_account_button_submit
                 : R.string.add_savings_account_button_next);
+        buttonPrevious.setVisibility(position <= 0
+                ? View.INVISIBLE
+                : View.VISIBLE);
     }
 }
