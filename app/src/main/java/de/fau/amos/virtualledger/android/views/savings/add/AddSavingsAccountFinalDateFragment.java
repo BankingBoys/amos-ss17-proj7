0abf7d47bb6725951a01bb20e326d0d0e04ce275
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-25 09:30:29 +0200

    Add fragment class for final date

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountFinalDateFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountFinalDateFragment.java
@@ -5,26 +5,24 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-
-import org.apache.commons.lang3.math.NumberUtils;
+import android.widget.DatePicker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.fau.amos.virtualledger.R;
 import de.fau.amos.virtualledger.dtos.AddSavingsAccountData;
 
-public class AddSavingsAccountAmountFragment extends AddSavingsAccountPage {
+public class AddSavingsAccountFinalDateFragment extends AddSavingsAccountPage {
     @SuppressWarnings("unused")
-    private static final String TAG = AddSavingsAccountAmountFragment.class.getSimpleName();
+    private static final String TAG = AddSavingsAccountFinalDateFragment.class.getSimpleName();
 
-    @BindView(R.id.add_savings_account_edit_text_amount)
-    EditText editText;
+    @BindView(R.id.add_savings_account_date_picker_enter_final_date)
+    DatePicker datePicker;
 
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.saving_accounts_add_fragment_amount, container, false);
+        final View view = inflater.inflate(R.layout.saving_accounts_add_fragment_final_date, container, false);
         ButterKnife.bind(this, view);
 
         return view;
@@ -32,6 +30,6 @@ public class AddSavingsAccountAmountFragment extends AddSavingsAccountPage {
 
     @Override
     public void fillInData(final AddSavingsAccountData addSavingsAccountResult) {
-        addSavingsAccountResult.goalBalance = NumberUtils.toDouble(editText.getText().toString());
+
     }
 }
