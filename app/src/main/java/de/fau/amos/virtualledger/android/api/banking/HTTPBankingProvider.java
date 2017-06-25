d6bf2355780783b4aa53e2557a41c501622497d0
simonschmoll <simon.schmoll@gmail.com>
2017-06-25 19:40:54 +0200

    added implementation with Rest Api call for adding saving Accounts

--- app/src/main/java/de/fau/amos/virtualledger/android/api/banking/HTTPBankingProvider.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/banking/HTTPBankingProvider.java
@@ -12,6 +12,7 @@ import de.fau.amos.virtualledger.dtos.BankAccess;
 import de.fau.amos.virtualledger.dtos.BankAccessCredential;
 import de.fau.amos.virtualledger.dtos.BankAccountSync;
 import de.fau.amos.virtualledger.dtos.BankAccountSyncResult;
+import de.fau.amos.virtualledger.dtos.StringApiModel;
 import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
 import retrofit2.Callback;
@@ -207,6 +208,36 @@ public class HTTPBankingProvider implements BankingProvider {
 
             @Override
             public void onFailure(retrofit2.Call<List<SavingsAccount>> call, Throwable t) {
+
+                Log.e(TAG, "No connection to server!");
+                observable.onError(new Throwable("No connection to server!"));
+            }
+        });
+
+        return observable;
+    }
+
+    @Override
+    public Observable<String> addSavingAccounts(List<SavingsAccount> savingsAccounts) {
+        final retrofit2.Call<Void> responseMessage = retrofit.create(Restapi.class).addSavingAccounts(authenticationProvider.getToken(), savingsAccounts);
+        final PublishSubject observable = PublishSubject.create();
+
+        responseMessage.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(retrofit2.Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    /*String callResponse = response.body();*/
+                    Log.v(TAG, "Adding Saving Accounts was successful " + response.code());
+                    observable.onNext("Adding Saving Accounts was successful");
+                } else {
+                    Log.e(TAG, "Adding Saving Accounts was not successful! ERROR " + response.code());
+                    observable.onError(new Throwable("Adding Saving Accounts was not successful!"));
+                }
+            }
+
+
+            @Override
+            public void onFailure(retrofit2.Call<Void> call, Throwable t) {
 
                 Log.e(TAG, "No connection to server!");
                 observable.onError(new Throwable("No connection to server!"));
