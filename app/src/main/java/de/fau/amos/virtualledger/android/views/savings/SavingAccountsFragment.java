b34f73f6fee434f4949381509bc65ff77b4b5232
simonschmoll <simon.schmoll@gmail.com>
2017-06-25 21:07:31 +0200

    implemented calling of bankingDataManager for retrieving the saving accounts

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingAccountsFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingAccountsFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,18 +15,36 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.api.banking.BankingProvider;
 import de.fau.amos.virtualledger.android.dagger.App;
+import de.fau.amos.virtualledger.android.data.BankingDataManager;
+import de.fau.amos.virtualledger.android.data.SyncFailedException;
 import de.fau.amos.virtualledger.android.model.SavingsAccount;
 import de.fau.amos.virtualledger.android.views.savings.add.AddSavingsAccountActivity;
+import de.fau.amos.virtualledger.dtos.BankAccess;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class SavingAccountsFragment extends Fragment {
     @SuppressWarnings("unused")
     private final String TAG = this.getClass().getSimpleName();
 
+    @Inject
+    BankingProvider bankingProvider;
+
     private ListAdapter adapter;
     private ListView savingsAccountList;
+    private List<SavingsAccount> savingsList;
+
+    @Inject
+    BankingDataManager bankingDataManager;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -34,6 +53,7 @@ public class SavingAccountsFragment extends Fragment {
 
         adapter = new SavingAccountsAdapter(getActivity(), R.id.savings_account_list, new ArrayList<SavingsAccount>());
         savingsAccountList.setAdapter(adapter);
+        savingsList = bankingDataManager.getSavingAccounts();
     }
 
     @Override
