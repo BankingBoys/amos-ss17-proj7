5282cc8e41b8449ec4125abd927a1534da4cbfc0
simonschmoll <simon.schmoll@gmail.com>
2017-06-26 20:52:13 +0200

    added setUp for future test methods

--- app/src/test/java/de/fau/amos/virtualledger/android/api/auth/MockedAuthenticationProviderTest.java
+++ app/src/test/java/de/fau/amos/virtualledger/android/api/auth/MockedAuthenticationProviderTest.java
@@ -1,8 +1,19 @@
 package de.fau.amos.virtualledger.android.api.auth;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Simon on 26.06.2017.
  */
 
 public class MockedAuthenticationProviderTest {
+
+    MockedAuthenticationProvider testProvider;
+
+    @Before
+    public void setUp() {
+        testProvider = new MockedAuthenticationProvider();
+    }
+
 }
