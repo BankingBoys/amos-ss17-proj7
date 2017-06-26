4fe33550189ed0eb97f59cf51af1fac920c5d38d
Georg <georg.schwarz@fau.de>
2017-06-26 12:14:00 +0200

    made simple test implementation of OidcAuthenticationActivity that uses custom UI now

--- app/src/main/java/de/fau/amos/virtualledger/android/authentication/oidc/OidcAuthenticationActivity.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/authentication/oidc/OidcAuthenticationActivity.java
@@ -1,10 +1,84 @@
 package de.fau.amos.virtualledger.android.authentication.oidc;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
 
-public class OidcAuthenticationActivity extends Activity {
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
+import de.fau.amos.virtualledger.android.api.auth.OidcAuthenticationProvider;
+import de.fau.amos.virtualledger.android.dagger.App;
+import de.fau.amos.virtualledger.android.dagger.component.OidcAuthenticationScope;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class OidcAuthenticationActivity extends AppCompatActivity {
     private static final String TAG = OidcAuthenticationActivity.class.getSimpleName();
 
 
+    @BindView(R.id.userIDField)
+    EditText userIdField;
 
+    @BindView(R.id.SecretField)
+    EditText secretField;
+
+    @Inject
+    AuthenticationProvider authenticationProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        ((App) getApplication()).getOidcAuthenticationComponent().inject(this);
+
+        setContentView(R.layout.authentication_activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.loginButton)
+    public void onLogin() {
+        final String userID = userIdField.getText().toString();
+        final String password = secretField.getText().toString();
+
+        // use observable due to asynchronizm
+        authenticationProvider.login(userID, password)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        if (authenticationProvider.isLoggedIn()) {
+
+                        } else {
+
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 }
