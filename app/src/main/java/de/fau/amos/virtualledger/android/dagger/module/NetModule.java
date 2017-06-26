c33297a23610efb3914f13ea4c316226bb835666
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-26 20:39:14 +0200

    Fixed gson being unable to parse date from server

--- app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/NetModule.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/NetModule.java
@@ -5,8 +5,13 @@ import android.app.Application;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 
-import javax.inject.Singleton;
+import java.lang.reflect.Type;
+import java.util.Date;
 
 import dagger.Module;
 import dagger.Provides;
@@ -54,6 +59,12 @@ public class NetModule {
     Gson provideGson() {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        gsonBuilder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public Date deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
         return gsonBuilder.create();
     }
 
