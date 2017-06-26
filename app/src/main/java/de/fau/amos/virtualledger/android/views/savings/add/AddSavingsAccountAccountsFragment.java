4e1d9d9f956869646364b96f44cf0fca45dfe303
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-06-26 19:12:31 +0200

    Fragment class for savings choosing accounts that are enabled for the transaction of money to the savings account

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountAccountsFragment.java
@@ -0,0 +1,36 @@
+package de.fau.amos.virtualledger.android.views.savings.add;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.dtos.AddSavingsAccountData;
+
+public class AddSavingsAccountAccountsFragment extends AddSavingsAccountPage {
+    @SuppressWarnings("unused")
+    private static final String TAG = AddSavingsAccountAccountsFragment.class.getSimpleName();
+
+    @OnClick(R.id.add_savings_account_button_accounts)
+    public void onClickChooseAccountsButton() {
+        //TODO
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.saving_accounts_add_fragment_accounts, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void fillInData(final AddSavingsAccountData addSavingsAccountResult) {
+        //TODO
+    }
+}
