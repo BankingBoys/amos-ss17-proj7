7267619bf661ddd9c1258181ade7f7e78a00d780
Georg <georg.schwarz@fau.de>
2017-06-26 10:59:07 +0200

    added some constants for configuration of authority endpoint and made clear register is not implemented in this class -> redirect in browser should do that

--- app/src/main/java/de/fau/amos/virtualledger/android/api/auth/OidcAuthenticationProvider.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/auth/OidcAuthenticationProvider.java
@@ -2,6 +2,8 @@ package de.fau.amos.virtualledger.android.api.auth;
 
 import android.content.Context;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 import io.reactivex.Observable;
 import retrofit2.Retrofit;
 
@@ -15,6 +17,12 @@ public class OidcAuthenticationProvider implements AuthenticationProvider {
 
     private Retrofit retrofit;
 
+    private final String CLIENT_ID = "multibanking-client";
+    private final String GRANT_TYPE_LOGIN = "password";
+    private final String GRANT_TYPE_REFRESH = "refresh_token";
+
+
+
     public OidcAuthenticationProvider(Retrofit retrofit) {
         this.retrofit = retrofit;
     }
@@ -22,7 +30,7 @@ public class OidcAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Observable<String> register(String email, String password, String firstname, String lastname) {
-        return null;
+        throw new NotImplementedException("Registering with OIDC is not implemented! Please redirect the user to the register page of the authority server in a browser!");
     }
 
     @Override
