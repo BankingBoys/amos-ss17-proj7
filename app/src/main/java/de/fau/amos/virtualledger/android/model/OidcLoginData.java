66ae4f7289120872b797fc6f51508f7af429c937
Georg <georg.schwarz@fau.de>
2017-06-26 11:41:18 +0200

    added constructors to OidcLoginData

--- app/src/main/java/de/fau/amos/virtualledger/android/model/OidcLoginData.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/model/OidcLoginData.java
@@ -10,4 +10,14 @@ public class OidcLoginData {
     public String username;
     public String password;
     public String grant_type;
+
+    public OidcLoginData(String client_id, String username, String password, String grant_type) {
+        this.client_id = client_id;
+        this.username = username;
+        this.password = password;
+        this.grant_type = grant_type;
+    }
+
+    public OidcLoginData() {
+    }
 }
