81ab34a05b973c6703745b5164c4fb320d96f29b
Georg <georg.schwarz@fau.de>
2017-06-26 10:56:24 +0200

    added model classes for communication with oidc authority (login + refresh token)

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/model/OidcLoginData.java
@@ -0,0 +1,13 @@
+package de.fau.amos.virtualledger.android.model;
+
+/**
+ * Created by Georg on 26.06.2017.
+ */
+
+public class OidcLoginData {
+
+    public String client_id;
+    public String username;
+    public String password;
+    public String grant_type;
+}
