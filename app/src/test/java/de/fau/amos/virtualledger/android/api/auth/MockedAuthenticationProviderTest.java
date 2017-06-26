1c62880652bc8d27550feca1d93def40bd1305c4
simonschmoll <simon.schmoll@gmail.com>
2017-06-26 21:02:15 +0200

    added test for register

--- app/src/test/java/de/fau/amos/virtualledger/android/api/auth/MockedAuthenticationProviderTest.java
+++ app/src/test/java/de/fau/amos/virtualledger/android/api/auth/MockedAuthenticationProviderTest.java
@@ -1,7 +1,11 @@
 package de.fau.amos.virtualledger.android.api.auth;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
+
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
 
 /**
  * Created by Simon on 26.06.2017.
@@ -15,5 +19,13 @@ public class MockedAuthenticationProviderTest {
     public void setUp() {
         testProvider = new MockedAuthenticationProvider();
     }
+
+    @Test
+    public void registerTest() {
+        Observable<String> testObservable = testProvider.register("testEmail", "testpw", "testfirstname", "testlastname");
+        Assertions.assertThat(testObservable).isNotNull();
+    }
+
+
 
 }
