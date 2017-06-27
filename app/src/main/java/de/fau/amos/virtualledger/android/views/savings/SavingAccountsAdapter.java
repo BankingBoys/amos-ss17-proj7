8bba1aa7ab057d9dd173db5aa989081ba9526f9d
simonschmoll <simon.schmoll@gmail.com>
2017-06-27 16:54:03 +0200

    cleaned  up imports

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingAccountsAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingAccountsAdapter.java
@@ -1,13 +1,11 @@
 package de.fau.amos.virtualledger.android.views.savings;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.support.v4.app.FragmentManager;
 
 import java.util.List;
 
