5afed45374c742c50c889c48d261d3bd3f9a7c43
simonschmoll <simon.schmoll@gmail.com>
2017-06-27 17:50:02 +0200

    added defaul constructor test

--- server/src/test/java/de/fau/amos/virtualledger/server/model/SavingAccountToUserTest.java
+++ server/src/test/java/de/fau/amos/virtualledger/server/model/SavingAccountToUserTest.java
@@ -1,7 +1,19 @@
 package de.fau.amos.virtualledger.server.model;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 /**
  * Created by Simon on 27.06.2017.
  */
 public class SavingAccountToUserTest {
+
+    @Test
+    public void constructorTest() {
+        SavingAccountToUserTest testAccount = new SavingAccountToUserTest();
+        Assertions.assertThat(testAccount).isNotNull();
+    }
+
+    
+
 }
