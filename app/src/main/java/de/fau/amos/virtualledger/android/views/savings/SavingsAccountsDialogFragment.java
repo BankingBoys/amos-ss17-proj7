460d0e82f06bae201ebaad58fcef9b35d9051ed7
simonschmoll <simon.schmoll@gmail.com>
2017-06-27 16:51:15 +0200

    new method for initializing the Dialog Fragment

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingsAccountsDialogFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingsAccountsDialogFragment.java
@@ -1,10 +1,29 @@
 package de.fau.amos.virtualledger.android.views.savings;
 
-import android.support.v4.app.DialogFragment;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.app.DialogFragment;
 
 /**
  * Created by Simon on 27.06.2017.
  */
 
 public class SavingsAccountsDialogFragment extends DialogFragment {
+
+    private static final String nameTag = "ACCOUNS_SAVINGS_NAME";
+
+    private String name = "testTitle";
+
+    public static SavingsAccountsDialogFragment newInstance(String name) {
+
+        SavingsAccountsDialogFragment savingsAccountsDialogFragment = new SavingsAccountsDialogFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(nameTag, name);
+        savingsAccountsDialogFragment.setArguments(bundle);
+        return savingsAccountsDialogFragment;
+    }
+    
 }
