698282a153ede4a8363c11a999550642f41d5ecf
simonschmoll <simon.schmoll@gmail.com>
2017-06-27 18:04:02 +0200

    added local variables for future tests

--- app/src/test/java/de/fau/amos/virtualledger/android/model/UserCredentialTest.java
+++ app/src/test/java/de/fau/amos/virtualledger/android/model/UserCredentialTest.java
@@ -1,8 +1,15 @@
 package de.fau.amos.virtualledger.android.model;
 
+import org.junit.Before;
+
 /**
  * Created by Simon on 27.06.2017.
  */
 
 public class UserCredentialTest {
+    String testEmail1 = "testEmail1";
+    String testPw1 = "testPW1";
+    String testFirstName1 = "testFirstName1";
+    String testLastName1 = "testLastName1";
+        
 }
