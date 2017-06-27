40c85d933f1c1c836e18d8064ac6b68fbb1626ae
simonschmoll <simon.schmoll@gmail.com>
2017-06-27 16:09:05 +0200

    added default Dialog Fragment for showing detailed informations about the savings account

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/SavingsAccountsDialogFragment.java
@@ -0,0 +1,10 @@
+package de.fau.amos.virtualledger.android.views.savings;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Simon on 27.06.2017.
+ */
+
+public class SavingsAccountsDialogFragment extends DialogFragment {
+}
