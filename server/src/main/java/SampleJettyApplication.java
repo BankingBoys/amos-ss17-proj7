7846761078cfc5c16d8946e780606e3b0a1698d5
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-28 14:52:20 +0200

    added spring boot tp jetty enviroment

---
+++ server/src/main/java/SampleJettyApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJettyApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleJettyApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/de/fau/amos/virtualledger/server/VirtualLedgerJettyApplication.java
@@ -0,0 +1,13 @@
+package de.fau.amos.virtualledger.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLedgerJettyApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(VirtualLedgerJettyApplication.class, args);
+	}
+
+}
