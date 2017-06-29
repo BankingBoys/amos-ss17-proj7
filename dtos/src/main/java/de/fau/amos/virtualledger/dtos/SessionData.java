fbc5befa356ce36ad1c6c21631bd37dffce38d67
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-06-29 23:36:24 +0200

    fixed checkstyle warnings

--- dtos/src/main/java/de/fau/amos/virtualledger/dtos/SessionData.java
+++ dtos/src/main/java/de/fau/amos/virtualledger/dtos/SessionData.java
@@ -5,65 +5,65 @@ import java.io.Serializable;
 /**
  *
  */
-public class SessionData implements Serializable{
-    /**
+public class SessionData implements Serializable {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	/**
-     *
-     */
-    private String email;
-    /**
-     *
-     */
-    private String sessionid;
+	 *
+	 */
+	private String email;
+	/**
+	 *
+	 */
+	private String sessionid;
 
-    /**
-     *
-     */
-    public SessionData() { }
+	/**
+	 *
+	 */
+	public SessionData() {
+	}
 
-    /**
-     *
-     * @param email
-     * @param token
-     */
-    public SessionData(String email, String token)
-    {
-        this.email = email;
-        this.sessionid = token;
-    }
+	/**
+	 *
+	 * @param email
+	 * @param token
+	 */
+	public SessionData(String email, String token) {
+		this.email = email;
+		this.sessionid = token;
+	}
 
-    /**
-     *
-     * @return
-     */
-    public String getEmail() {
-        return email;
-    }
+	/**
+	 *
+	 * @return
+	 */
+	public String getEmail() {
+		return email;
+	}
 
-    /**
-     *
-     * @param email
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	/**
+	 *
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    /**
-     *
-     * @return
-     */
-    public String getSessionid() {
-        return sessionid;
-    }
+	/**
+	 *
+	 * @return
+	 */
+	public String getSessionid() {
+		return sessionid;
+	}
 
-    /**
-     *
-     * @param sessionid
-     */
-    public void setSessionid(String sessionid) {
-        this.sessionid = sessionid;
-    }
+	/**
+	 *
+	 * @param sessionid
+	 */
+	public void setSessionid(String sessionid) {
+		this.sessionid = sessionid;
+	}
 }
--- dtos/src/test/java/dtos/BankAccessCredentialTest.java
+++ dtos/src/test/java/dtos/BankAccessCredentialTest.java
@@ -1,65 +1,67 @@
 package dtos;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import de.fau.amos.virtualledger.dtos.BankAccessCredential;
-import org.junit.*;
+
 /**
  * Created by Simon on 05.06.2017.
  */
+
 public class BankAccessCredentialTest {
 
-    private String bankcode = "testCode";
-    private String banklogin = "testLogin";
-    private String pin = "testPin";
-    private BankAccessCredential credential;
+	private String bankcode = "testCode";
+	private String banklogin = "testLogin";
+	private String pin = "testPin";
+	private BankAccessCredential credential;
 
-    /**
-     *
-     */
-    @Before
-    public void setUp() {
-        credential = new BankAccessCredential(bankcode, banklogin, pin);
-    }
+	/**
+	 *
+	 */
+	@Before
+	public void setUp() {
+		credential = new BankAccessCredential(bankcode, banklogin, pin);
+	}
 
-    /**
-     *
-     */
-    @Test
-    public void constructorTest() {
-        BankAccessCredential credential = new BankAccessCredential(bankcode, banklogin, pin);
-        Assert.assertNotNull(credential);
-    }
+	/**
+	 *
+	 */
+	@Test
+	public void constructorTest() {
+		BankAccessCredential credential = new BankAccessCredential(bankcode, banklogin, pin);
+		Assert.assertNotNull(credential);
+	}
 
-    /**
-     *
-     */
-    @Test
-    public void getAndSetBankcodeTest() {
-        String testCode = "newTestCode";
-        credential.setBankcode(testCode);
-        Assert.assertEquals(testCode, credential.getBankcode());
-    }
+	/**
+	 *
+	 */
+	@Test
+	public void getAndSetBankcodeTest() {
+		String testCode = "newTestCode";
+		credential.setBankcode(testCode);
+		Assert.assertEquals(testCode, credential.getBankcode());
+	}
 
-    /**
-     *
-     */
-    @Test
-    public void getAndSetBankloginTest() {
-        String testLogin = "newTestLogin";
-        credential.setBanklogin(testLogin);
-        Assert.assertEquals(testLogin, credential.getBanklogin());
-    }
+	/**
+	 *
+	 */
+	@Test
+	public void getAndSetBankloginTest() {
+		String testLogin = "newTestLogin";
+		credential.setBanklogin(testLogin);
+		Assert.assertEquals(testLogin, credential.getBanklogin());
+	}
 
-    /**
-     *
-     */
-    @Test
-    public void getAndSetPinTest() {
-        String testPin = "newTestPin";
-        credential.setPin(testPin);
-        Assert.assertEquals(testPin, credential.getPin());
-    }
-
-
-
+	/**
+	 *
+	 */
+	@Test
+	public void getAndSetPinTest() {
+		String testPin = "newTestPin";
+		credential.setPin(testPin);
+		Assert.assertEquals(testPin, credential.getPin());
+	}
 
 }
