d7a0c07b27d9e2e8454a2647bd87d491da5c526a
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 11:13:07 +0200

    added new package with default class for the contact screen

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsFragment.java
@@ -0,0 +1,10 @@
+package de.fau.amos.virtualledger.android.views.contacts;
+
+import android.app.Fragment;
+
+/**
+ * Created by Simon on 01.07.2017.
+ */
+
+public class ContactsFragment extends Fragment {
+}
