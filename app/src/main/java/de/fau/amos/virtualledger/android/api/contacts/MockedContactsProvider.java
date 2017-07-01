3229713a9c98155c62974e4ce23918a471baa3bd
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 13:50:52 +0200

    implemented getContacts for mocking the GET Rest Api Call

--- app/src/main/java/de/fau/amos/virtualledger/android/api/contacts/MockedContactsProvider.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/contacts/MockedContactsProvider.java
@@ -1,9 +1,11 @@
 package de.fau.amos.virtualledger.android.api.contacts;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.fau.amos.virtualledger.android.model.Contact;
 import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by Simon on 01.07.2017.
@@ -11,9 +13,34 @@ import io.reactivex.Observable;
 
 public class MockedContactsProvider implements ContactsProvider {
 
+    public static final int DELAY_TIME_MILLISECONDS = 300;
+
     @Override
     public Observable<List<Contact>> getContacts() {
-        return null;
+
+        final List<Contact> contactsList = new ArrayList<>();
+
+        Contact contact= new Contact("dummyEmail", "dummyfirst", "dummylast");
+        contactsList.add(contact);
+
+        final PublishSubject observable = PublishSubject.create();
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //Wait until subject is subscribed
+                    Thread.sleep(DELAY_TIME_MILLISECONDS);
+                } catch (Exception e) {
+
+                }
+                // publish accounts to subject
+                observable.onNext(contactsList);
+                observable.onComplete();
+            }
+        });
+        th.start();
+
+        return observable;
     }
 
     @Override
