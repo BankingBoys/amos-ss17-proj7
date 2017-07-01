fffc1227e44302fccd2a627bf6421ed1679c49d0
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 12:38:04 +0200

    added attributes and default implementation of onActivityCreated

--- app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsFragment.java
@@ -1,10 +1,20 @@
 package de.fau.amos.virtualledger.android.views.contacts;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ListView;
 
 /**
  * Created by Simon on 01.07.2017.
  */
 
 public class ContactsFragment extends Fragment {
+    private ContactsAdapter adapter;
+    private ListView contactListView;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
