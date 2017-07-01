c2cc5762518b3943f5b21fa0f801808a04d3744f
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 11:57:58 +0200

    added safety check for getView, so that the returned view is not null for the case that the input view might be null

--- app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
@@ -3,6 +3,7 @@ package de.fau.amos.virtualledger.android.views.contacts;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -28,6 +29,12 @@ public class ContactsAdapter extends ArrayAdapter<Contact> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.contacts_list_item, parent, false);
+        }
         return convertView;
     }
+
+    
 }
