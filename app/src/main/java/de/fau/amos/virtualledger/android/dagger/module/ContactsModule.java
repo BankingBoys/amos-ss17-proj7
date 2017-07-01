0c6e0bbd8e0d1e951639046bba94294c6049212e
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 14:37:54 +0200

    added new ContactModule for providing the HTTPContactsProvider

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/ContactsModule.java
@@ -0,0 +1,23 @@
+package de.fau.amos.virtualledger.android.dagger.module;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
+import de.fau.amos.virtualledger.android.api.contacts.ContactsProvider;
+import de.fau.amos.virtualledger.android.api.contacts.HTTPContactsProvider;
+import retrofit2.Retrofit;
+
+/**
+ * Created by Simon on 01.07.2017.
+ */
+
+@Module(includes = {NetModule.class, AuthenticationModule.class})
+public class ContactsModule {
+    @Provides
+    @Singleton
+    ContactsProvider provideContactsProvider(Retrofit retrofit, AuthenticationProvider authenticationProvider) {
+        return new HTTPContactsProvider(retrofit, authenticationProvider);
+    }
+}
