7e02dbdb6eab224a60e41b44dd866938aed9cd87
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 12:14:46 +0200

    TextView Name is now filled with the passed parameter from Contacts

--- app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -33,8 +36,12 @@ public class ContactsAdapter extends ArrayAdapter<Contact> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.contacts_list_item, parent, false);
         }
+        Contact contact = super.getItem(position);
+
+        TextView contactName = (TextView) convertView.findViewById(R.id.id_contactName);
+        contactName.setText(contact.getFirstName() + " " + contact.getLastName());
         return convertView;
     }
 
-    
+
 }
