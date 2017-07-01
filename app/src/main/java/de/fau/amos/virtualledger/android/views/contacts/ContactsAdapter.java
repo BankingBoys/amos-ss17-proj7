17099d2d7a33200dc0dc199767e65eda55318d07
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 11:22:50 +0200

    added attribute activity and constructor to the contact adapter

--- app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
@@ -1,8 +1,23 @@
 package de.fau.amos.virtualledger.android.views.contacts;
 
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.model.Contact;
+
 /**
  * Created by Simon on 01.07.2017.
  */
 
-public class ContactsAdapter {
+public class ContactsAdapter extends ArrayAdapter<Contact> {
+
+    private Activity activity;
+
+    public ContactsAdapter(Activity activity, int layout, List<Contact> contacts) {
+        super(activity, layout, contacts);
+        this.activity = activity;
+    }
 }
