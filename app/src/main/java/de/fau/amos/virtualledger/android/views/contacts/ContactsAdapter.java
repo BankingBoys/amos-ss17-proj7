364a7619a964329415817b0e8588b0786594f375
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 11:25:01 +0200

    added default method for getView of the adapter

--- app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/ContactsAdapter.java
@@ -1,6 +1,10 @@
 package de.fau.amos.virtualledger.android.views.contacts;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -19,5 +23,11 @@ public class ContactsAdapter extends ArrayAdapter<Contact> {
     public ContactsAdapter(Activity activity, int layout, List<Contact> contacts) {
         super(activity, layout, contacts);
         this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return convertView;
     }
 }
