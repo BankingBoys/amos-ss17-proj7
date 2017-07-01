53483ff95e21fb8d11a8347acf5f67d2ad29d8b9
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 13:37:51 +0200

    implementing the ContactProvider interface with default methods

--- app/src/main/java/de/fau/amos/virtualledger/android/api/contacts/HTTPContactsProvider.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/contacts/HTTPContactsProvider.java
@@ -1,13 +1,24 @@
 package de.fau.amos.virtualledger.android.api.contacts;
 
+import android.util.Log;
+
+import java.util.List;
+
+import de.fau.amos.virtualledger.android.api.Restapi;
 import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
+import de.fau.amos.virtualledger.android.model.Contact;
+import de.fau.amos.virtualledger.android.model.SavingsAccount;
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 /**
  * Created by Simon on 01.07.2017.
  */
 
-public class HTTPContactsProvider {
+public class HTTPContactsProvider implements ContactsProvider{
 
     private static final String TAG = "HTTPContactsProvider";
 
@@ -19,4 +30,13 @@ public class HTTPContactsProvider {
         this.authenticationProvider = authenticationProvider;
     }
 
+    @Override
+    public Observable<List<Contact>> getContacts() {
+        return null;
+    }
+
+    @Override
+    public Observable<String> addContacts(Contact savingsAccount) {
+        return null;
+    }
 }
