a0eaa5783580ac1ec13008f7444a43cb929cac7d
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 16:25:18 +0200

    added new test for adding saving accounts

--- app/src/test/java/de/fau/amos/virtualledger/android/api/savings/MockedSavingsProviderTest.java
+++ app/src/test/java/de/fau/amos/virtualledger/android/api/savings/MockedSavingsProviderTest.java
@@ -2,7 +2,12 @@ package de.fau.amos.virtualledger.android.api.savings;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+import de.fau.amos.virtualledger.android.model.SavingsAccount;
 import io.reactivex.Observable;
 
 /**
@@ -22,4 +27,11 @@ public class MockedSavingsProviderTest {
         Observable testObservable = mockedSavingsProvider.getSavingAccounts();
         assertThat(testObservable).isNotNull();
     }
+
+    @Test
+    public void addSavingAccount() {
+        Observable testObservable = mockedSavingsProvider.addSavingAccount(new SavingsAccount("1","name", 0, 0, new Date(12312)));
+        assertThat(testObservable).isNotNull();
+    }
+
 }
