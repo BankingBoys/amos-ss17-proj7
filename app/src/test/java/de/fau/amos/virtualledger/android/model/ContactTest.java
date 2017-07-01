703d654e845e398be724808d4f24bed00643edb1
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 12:19:45 +0200

    added new test class with private attributes for the model class contact

--- app/src/test/java/de/fau/amos/virtualledger/android/model/ContactTest.java
+++ app/src/test/java/de/fau/amos/virtualledger/android/model/ContactTest.java
@@ -1,8 +1,17 @@
 package de.fau.amos.virtualledger.android.model;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Simon on 01.07.2017.
  */
 
+
 public class ContactTest {
+    private Contact contact;
+    private String email = "testEmail";
+    private String firstName = "testFirstName";
+    private String lastName = "testLastName";
+    
 }
