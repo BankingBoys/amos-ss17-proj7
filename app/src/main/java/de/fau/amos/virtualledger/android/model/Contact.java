23a58b949e716c49aabe04cc842af08420188f60
simonschmoll <simon.schmoll@gmail.com>
2017-07-01 11:19:32 +0200

    added getter and setter for the contact attributes

--- app/src/main/java/de/fau/amos/virtualledger/android/model/Contact.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/model/Contact.java
@@ -10,4 +10,35 @@ public class Contact {
     private String firstName;
     private String lastName;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
