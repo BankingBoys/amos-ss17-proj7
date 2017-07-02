e89f183a878b63d8be1cc86f37b7c477cc4fc8a6
simonschmoll <simon.schmoll@gmail.com>
2017-07-02 12:39:34 +0200

    implements now Serializable

--- dtos/src/main/java/de/fau/amos/virtualledger/dtos/BankAccountComparator.java
+++ dtos/src/main/java/de/fau/amos/virtualledger/dtos/BankAccountComparator.java
@@ -1,9 +1,12 @@
 package de.fau.amos.virtualledger.dtos;
 
+import java.io.Serializable;
 import java.util.Comparator;
 import java.util.Locale;
 
-public class BankAccountComparator implements Comparator<BankAccount> {
+public class BankAccountComparator implements Comparator<BankAccount>, Serializable {
+
+    private static final long serialVersionUID = 2345678910L;
 
     @Override
     public int compare(BankAccount access1, BankAccount access2) {
