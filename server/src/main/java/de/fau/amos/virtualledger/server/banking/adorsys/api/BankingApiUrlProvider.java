9740fa98b3fe2f4ca61e8cf859f50a45622ba8fe
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 12:36:36 +0200

    checkstyle: fixed checkstyle violations in BankingApiUrlProvider

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiUrlProvider.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiUrlProvider.java
@@ -1,12 +1,11 @@
 package de.fau.amos.virtualledger.server.banking.adorsys.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * Class for providing URLs for adorsys api.
- * Uses the BankingApiConfiguration for building the URLs
+ * Class for providing URLs for adorsys api. Uses the BankingApiConfiguration
+ * for building the URLs
  */
 @Component
 
@@ -14,40 +13,34 @@ public class BankingApiUrlProvider {
 
     private BankingApiConfiguration configuration;
 
-
     @Autowired
-    public BankingApiUrlProvider(BankingApiConfiguration configuration)
-    {
+    public BankingApiUrlProvider(BankingApiConfiguration configuration) {
         this.configuration = configuration;
     }
+
     // protected empty constructor required for server to init it
     protected BankingApiUrlProvider() {
         this(null);
     }
 
-
-    public String getUserEndpointUrl()
-    {
+    public String getUserEndpointUrl() {
         return configuration.getBankingApiUrlAbsolute() + configuration.getUserApiUrlRelative();
     }
 
-    public String getBankAccessEndpointUrl(String userId)
-    {
+    public String getBankAccessEndpointUrl(String userId) {
         String url = configuration.getBankingApiUrlAbsolute() + configuration.getBankAccessApiUrlRelative();
         url = url.replaceAll("\\{userId\\}", userId);
         return url;
     }
 
-    public String getBankAccountEndpointUrl(String userId, String bankAccessId)
-    {
+    public String getBankAccountEndpointUrl(String userId, String bankAccessId) {
         String url = configuration.getBankingApiUrlAbsolute() + configuration.getBankAccountApiUrlRelative();
         url = url.replaceAll("\\{userId\\}", userId);
         url = url.replaceAll("\\{accessId\\}", bankAccessId);
         return url;
     }
 
-    public String getBankAccountSyncEndpointUrl(String userId, String bankAccessId, String bankAccountId)
-    {
+    public String getBankAccountSyncEndpointUrl(String userId, String bankAccessId, String bankAccountId) {
         String url = configuration.getBankingApiUrlAbsolute() + configuration.getBankAccountSyncApiUrlRelative();
         url = url.replaceAll("\\{userId\\}", userId);
         url = url.replaceAll("\\{accessId\\}", bankAccessId);
