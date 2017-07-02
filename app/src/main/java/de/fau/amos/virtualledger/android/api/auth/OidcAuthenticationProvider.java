13d014753c964752d40234366ad10d9d96ca7318
Georg <georg.schwarz@fau.de>
2017-07-02 18:16:32 +0200

    changed token returned from OidcAuthenticationProvider to already contain prefix "Bearer "

--- app/src/main/java/de/fau/amos/virtualledger/android/api/auth/OidcAuthenticationProvider.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/auth/OidcAuthenticationProvider.java
@@ -201,7 +201,7 @@ public class OidcAuthenticationProvider implements AuthenticationProvider {
                     .subscribe(new Consumer<String>() {
                         @Override
                         public void accept(@NonNull String s) throws Exception {
-                            observable.onNext(oidcData.access_token);
+                            observable.onNext("Bearer " + oidcData.access_token);
                         }
                     }, new Consumer<Throwable>() {
                         @Override
@@ -213,7 +213,7 @@ public class OidcAuthenticationProvider implements AuthenticationProvider {
                     });
             return observable;
         } else {
-            return Observable.just(oidcData.access_token);
+            return Observable.just("Bearer " + oidcData.access_token);
         }
     }
 
