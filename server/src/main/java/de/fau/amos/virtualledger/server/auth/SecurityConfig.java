929e98ab97541071d358216b8aca21ac23bccdca
Georg <georg.schwarz@fau.de>
2017-07-02 15:54:19 +0200

    removed unnecessary SecuredFilter from server

--- server/src/main/java/de/fau/amos/virtualledger/server/auth/SecurityConfig.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/auth/SecurityConfig.java
@@ -1,7 +1,7 @@
 package de.fau.amos.virtualledger.server.auth;
 
 
-import org.keycloak.adapters.KeycloakConfigResolver;
+import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
 
 @Configuration
 @EnableWebSecurity
