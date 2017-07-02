54e782bca19cfbcaae2db409409221dc435c363f
Georg <georg.schwarz@fau.de>
2017-07-02 18:46:10 +0200

    applied checkstyle to * import

--- server/src/main/java/de/fau/amos/virtualledger/server/api/BankingOverviewApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/BankingOverviewApiEndpoint.java
@@ -14,7 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
