251fb2f9d0696d569ed1cdff6424a276fb1269fd
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 13:06:59 +0200

    checkstyle: fixed checkstyle-violations in >SampleController. Unused imports, code formatting

--- server/src/main/java/de/fau/amos/virtualledger/server/jetty_test/SampleController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/jetty_test/SampleController.java
@@ -1,12 +1,9 @@
 package de.fau.amos.virtualledger.server.jetty_test;
 
-import de.fau.amos.virtualledger.dtos.BankAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 public class SampleController {
