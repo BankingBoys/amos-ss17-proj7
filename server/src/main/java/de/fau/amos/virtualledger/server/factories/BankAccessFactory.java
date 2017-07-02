a6d279235e0ace9f7a5ad7a42c9e48e2c10b5783
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 13:02:20 +0200

    fixed checkstyle violations in BankAccessFactory

--- server/src/main/java/de/fau/amos/virtualledger/server/factories/BankAccessFactory.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/factories/BankAccessFactory.java
@@ -3,28 +3,25 @@ package de.fau.amos.virtualledger.server.factories;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import de.fau.amos.virtualledger.dtos.BankAccess;
 import de.fau.amos.virtualledger.server.banking.model.BankAccessBankingModel;
 
-
 @Component
 
 public class BankAccessFactory {
 
-    public BankAccess createBankAccess(BankAccessBankingModel bankingModel)
-    {
-        BankAccess bankAccess = new BankAccess(bankingModel.getId(), bankingModel.getBankName(), bankingModel.getBankCode(), bankingModel.getBankLogin());
+    public BankAccess createBankAccess(BankAccessBankingModel bankingModel) {
+        BankAccess bankAccess = new BankAccess(bankingModel.getId(), bankingModel.getBankName(),
+                bankingModel.getBankCode(), bankingModel.getBankLogin());
         return bankAccess;
     }
 
-    public List<BankAccess> createBankAccesses(List<BankAccessBankingModel> bankingModelList)
-    {
+    public List<BankAccess> createBankAccesses(List<BankAccessBankingModel> bankingModelList) {
         List<BankAccess> bankAccessesResult = new ArrayList<BankAccess>();
 
-        for (BankAccessBankingModel bankingModel: bankingModelList) {
+        for (BankAccessBankingModel bankingModel : bankingModelList) {
             bankAccessesResult.add(this.createBankAccess(bankingModel));
         }
         return bankAccessesResult;
