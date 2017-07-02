f5876534de6907758f29d87964001a5eaed4a1dd
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 12:26:39 +0200

    checkstyle: fixed checkstyle violations in SessionIdGenerator

--- server/src/main/java/de/fau/amos/virtualledger/server/auth/SessionIdGenerator.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/auth/SessionIdGenerator.java
@@ -8,11 +8,12 @@ import java.util.Base64;
 @Component
 class SessionIdGenerator {
 
+    private static final int RANDOM_BYTE_LENGTH = 128;
     private final SecureRandom secureRandom = new SecureRandom();
     private Base64.Encoder encoder = Base64.getEncoder();
 
     String generate() {
-        final byte[] bytes = new byte[128];
+        final byte[] bytes = new byte[RANDOM_BYTE_LENGTH];
         secureRandom.nextBytes(bytes);
         return encoder.encodeToString(bytes);
     }
