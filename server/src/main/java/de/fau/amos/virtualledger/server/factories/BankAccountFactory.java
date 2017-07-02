9f5a7fc2e015a4c70347afb2eecac6f676a15e6c
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 13:03:00 +0200

    checkstyle: removed unused import, formatted code

--- server/src/main/java/de/fau/amos/virtualledger/server/factories/BankAccountFactory.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/factories/BankAccountFactory.java
@@ -3,22 +3,18 @@ package de.fau.amos.virtualledger.server.factories;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import de.fau.amos.virtualledger.dtos.BankAccount;
 import de.fau.amos.virtualledger.server.banking.model.BankAccountBankingModel;
 
-
 @Component
 
 public class BankAccountFactory {
 
-    public BankAccount createBankAccount(BankAccountBankingModel bankingModel)
-    {
+    public BankAccount createBankAccount(BankAccountBankingModel bankingModel) {
         double balance = 0;
-        if(bankingModel.getBankAccountBalance() != null)
-        {
+        if (bankingModel.getBankAccountBalance() != null) {
             balance = bankingModel.getBankAccountBalance().getReadyHbciBalance();
         }
 
@@ -26,11 +22,10 @@ public class BankAccountFactory {
         return bankAccount;
     }
 
-    public List<BankAccount> createBankAccounts(List<BankAccountBankingModel> bankingModelList)
-    {
+    public List<BankAccount> createBankAccounts(List<BankAccountBankingModel> bankingModelList) {
         List<BankAccount> bankAccountsResult = new ArrayList<BankAccount>();
 
-        for (BankAccountBankingModel bankingModel: bankingModelList) {
+        for (BankAccountBankingModel bankingModel : bankingModelList) {
             bankAccountsResult.add(this.createBankAccount(bankingModel));
         }
         return bankAccountsResult;
