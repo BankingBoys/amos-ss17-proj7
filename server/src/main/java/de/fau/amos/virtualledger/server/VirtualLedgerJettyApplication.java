663d54c6fe96a84563a6b75a2a2106fec2134f02
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 12:06:03 +0200

    checkstyle: fixed visibility and whitespace

--- server/src/main/java/de/fau/amos/virtualledger/server/VirtualLedgerJettyApplication.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/VirtualLedgerJettyApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VirtualLedgerJettyApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(VirtualLedgerJettyApplication.class, args);
-	}
+    private VirtualLedgerJettyApplication() {
+    }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(VirtualLedgerJettyApplication.class, args);
+    }
 }
