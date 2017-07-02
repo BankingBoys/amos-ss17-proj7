60881d6638771c96419830289b5ec2864073f322
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 13:26:33 +0200

    removed public fields with getter and setter

--- server/src/main/java/de/fau/amos/virtualledger/server/model/SavingsAccount.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/SavingsAccount.java
@@ -1,7 +1,12 @@
 package de.fau.amos.virtualledger.server.model;
 
-import javax.persistence.*;
 import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="SavingsAccounts")
--- server/src/main/java/de/fau/amos/virtualledger/server/model/SavingsAccountToUser.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/SavingsAccountToUser.java
@@ -8,15 +8,39 @@ import javax.persistence.*;
 public class SavingsAccountToUser {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id;
-    public String email;
-    public int id_savingsaccount;
+    private int id;
+    private String email;
+    private int idSavingsaccount;
 
-    public SavingsAccountToUser(String email, int id_savingsaccount) {
-        this.email = email;
-        this.id_savingsaccount = id_savingsaccount;
+    public SavingsAccountToUser(String email, int idSavingsaccount) {
+        this.setEmail(email);
+        this.setId_savingsaccount(idSavingsaccount);
     }
 
     public SavingsAccountToUser() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId_savingsaccount() {
+        return this.idSavingsaccount;
+    }
+
+    public void setId_savingsaccount(int id_savingsaccount) {
+        this.idSavingsaccount = id_savingsaccount;
     }
 }
