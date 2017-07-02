9351aa18a5389dcc9761bff83805d2fcba7436d6
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 12:25:28 +0200

    checkstyle: fixed checkstyle violations in SecurityConfig

--- server/src/main/java/de/fau/amos/virtualledger/server/auth/SecurityConfig.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/auth/SecurityConfig.java
@@ -1,23 +1,21 @@
 package de.fau.amos.virtualledger.server.auth;
 
 
-import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserCredentialRepository userCredentialRepository;
+    private UserCredentialRepository userCredentialRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
