d1acf010e9f0c46846af82f235945217f37af2b6
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 13:13:32 +0200

    checkstyle: fixed : DeletedBankAccessesRepository

--- server/src/main/java/de/fau/amos/virtualledger/server/persistence/DeletedBankAccessesRepository.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/persistence/DeletedBankAccessesRepository.java
@@ -1,90 +1,92 @@
 package de.fau.amos.virtualledger.server.persistence;
 
-import de.fau.amos.virtualledger.server.model.DeletedBankAccess;
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.*;
-import java.lang.invoke.MethodHandles;
-import java.util.List;
+import de.fau.amos.virtualledger.server.model.DeletedBankAccess;
 
 @Component
 
 public class DeletedBankAccessesRepository {
-    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    EntityManagerFactory entityManagerFactory;
+    private EntityManagerFactory entityManagerFactory;
 
     @Autowired
     public DeletedBankAccessesRepository(EntityManagerFactoryProvider entityManagerFactoryProvider) {
         this.entityManagerFactory = entityManagerFactoryProvider.getEntityManagerFactory();
     }
-    protected DeletedBankAccessesRepository() { }
 
-    public void createDeletedBankAccess(DeletedBankAccess deletedBankAccess)
-    {
+    protected DeletedBankAccessesRepository() {
+    }
+
+    public void createDeletedBankAccess(DeletedBankAccess deletedBankAccess) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
+        try {
             EntityTransaction entityTransaction = entityManager.getTransaction();
             try {
                 entityTransaction.begin();
                 entityManager.persist(deletedBankAccess);
                 entityTransaction.commit();
-            } catch(EntityExistsException entityExistsException) {
-                logger.info("Entity already exists: "+ deletedBankAccess);
+            } catch (EntityExistsException entityExistsException) {
+                LOGGER.info("Entity already exists: " + deletedBankAccess);
                 entityTransaction.rollback();
                 throw entityExistsException;
-            } catch(IllegalArgumentException persistenceException) {
-                logger.warn("", persistenceException);
+            } catch (IllegalArgumentException persistenceException) {
+                LOGGER.warn("", persistenceException);
                 entityTransaction.rollback();
                 throw persistenceException;
             }
-        }
-        finally {
+        } finally {
             entityManager.close();
         }
     }
 
-    public void deleteDeletedBankAccessByEmailAndId(final String email, final String accessId)
-    {
+    public void deleteDeletedBankAccessByEmailAndId(final String email, final String accessId) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        try{
+        try {
             final EntityTransaction entityTransaction = entityManager.getTransaction();
             try {
                 entityTransaction.begin();
 
-                Query query = entityManager.createQuery("Select s FROM DeletedBankAccess s WHERE s.userEmail = :email AND s.bankAccessId = :accessId");
+                Query query = entityManager.createQuery(
+                        "Select s FROM DeletedBankAccess s WHERE s.userEmail = :email AND s.bankAccessId = :accessId");
                 query.setParameter("email", email);
                 query.setParameter("accessId", accessId);
                 List<DeletedBankAccess> deletedBankAccessList = query.getResultList();
 
-                for(int i = 0; i < deletedBankAccessList.size(); ++i)
-                {
+                for (int i = 0; i < deletedBankAccessList.size(); ++i) {
                     DeletedBankAccess deletedBankAccess = deletedBankAccessList.get(i);
                     entityManager.remove(deletedBankAccess);
                 }
                 entityTransaction.commit();
-            } catch(final Exception e) {
-                logger.warn("", e);
+            } catch (final Exception e) {
+                LOGGER.warn("", e);
                 entityTransaction.rollback();
                 throw e;
             }
-        }
-        finally {
+        } finally {
             entityManager.close();
         }
     }
 
-
-    public List<DeletedBankAccess> getDeletedBankAccessIdsByEmail(final String email)
-    {
+    public List<DeletedBankAccess> getDeletedBankAccessIdsByEmail(final String email) {
         final EntityManager entityManager = entityManagerFactory.createEntityManager();
         try {
-            final Query query = entityManager.createQuery("Select u FROM DeletedBankAccess u WHERE u.userEmail = :email");
+            final Query query = entityManager
+                    .createQuery("Select u FROM DeletedBankAccess u WHERE u.userEmail = :email");
             query.setParameter("email", email);
             final List resultList = query.getResultList();
             return resultList;
