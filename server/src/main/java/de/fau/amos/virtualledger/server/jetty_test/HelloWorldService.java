744169395807cd44d4873a827c06c2afff64e3d3
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 13:05:48 +0200

    checkstyle: fixed checkstyle vioaltions in HelloWorldService

--- server/src/main/java/de/fau/amos/virtualledger/server/jetty_test/HelloWorldService.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/jetty_test/HelloWorldService.java
@@ -1,23 +1,16 @@
 package de.fau.amos.virtualledger.server.jetty_test;
 
-import de.fau.amos.virtualledger.dtos.BankAccess;
-import de.fau.amos.virtualledger.server.banking.BankingOverviewController;
-import de.fau.amos.virtualledger.server.banking.model.BankingException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class HelloWorldService {
 
-	@Value("${name:User}")
-	private String name;
+    @Value("${name:User}")
+    private String name;
 
-	public String getHelloMessage() {
-		return name;
-	}
+    public String getHelloMessage() {
+        return name;
+    }
 
 }
