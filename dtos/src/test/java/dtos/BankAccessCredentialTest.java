3ac871963739ac920e3023f36a9a8f529efeb2b6
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 11:30:14 +0200

    fixed formatting: spaces instead of tabs

--- dtos/src/test/java/dtos/BankAccessCredentialTest.java
+++ dtos/src/test/java/dtos/BankAccessCredentialTest.java
@@ -12,56 +12,56 @@ import de.fau.amos.virtualledger.dtos.BankAccessCredential;
 
 public class BankAccessCredentialTest {
 
-	private String bankcode = "testCode";
-	private String banklogin = "testLogin";
-	private String pin = "testPin";
-	private BankAccessCredential credential;
+    private String bankcode = "testCode";
+    private String banklogin = "testLogin";
+    private String pin = "testPin";
+    private BankAccessCredential credential;
 
-	/**
-	 *
-	 */
-	@Before
-	public void setUp() {
-		credential = new BankAccessCredential(bankcode, banklogin, pin);
-	}
+    /**
+     *
+     */
+    @Before
+    public void setUp() {
+        credential = new BankAccessCredential(bankcode, banklogin, pin);
+    }
 
-	/**
-	 *
-	 */
-	@Test
-	public void constructorTest() {
-		BankAccessCredential credential = new BankAccessCredential(bankcode, banklogin, pin);
-		Assert.assertNotNull(credential);
-	}
+    /**
+     *
+     */
+    @Test
+    public void constructorTest() {
+        BankAccessCredential credential = new BankAccessCredential(bankcode, banklogin, pin);
+        Assert.assertNotNull(credential);
+    }
 
-	/**
-	 *
-	 */
-	@Test
-	public void getAndSetBankcodeTest() {
-		String testCode = "newTestCode";
-		credential.setBankcode(testCode);
-		Assert.assertEquals(testCode, credential.getBankcode());
-	}
+    /**
+     *
+     */
+    @Test
+    public void getAndSetBankcodeTest() {
+        String testCode = "newTestCode";
+        credential.setBankcode(testCode);
+        Assert.assertEquals(testCode, credential.getBankcode());
+    }
 
-	/**
-	 *
-	 */
-	@Test
-	public void getAndSetBankloginTest() {
-		String testLogin = "newTestLogin";
-		credential.setBanklogin(testLogin);
-		Assert.assertEquals(testLogin, credential.getBanklogin());
-	}
+    /**
+     *
+     */
+    @Test
+    public void getAndSetBankloginTest() {
+        String testLogin = "newTestLogin";
+        credential.setBanklogin(testLogin);
+        Assert.assertEquals(testLogin, credential.getBanklogin());
+    }
 
-	/**
-	 *
-	 */
-	@Test
-	public void getAndSetPinTest() {
-		String testPin = "newTestPin";
-		credential.setPin(testPin);
-		Assert.assertEquals(testPin, credential.getPin());
-	}
+    /**
+     *
+     */
+    @Test
+    public void getAndSetPinTest() {
+        String testPin = "newTestPin";
+        credential.setPin(testPin);
+        Assert.assertEquals(testPin, credential.getPin());
+    }
 
 }
