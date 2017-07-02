021cabfeca8b6b63b42a595de339ffc1f2d76086
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-02 12:35:59 +0200

    checkstyle: fixed checkstryle-violations in BankingApiFacade

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiFacade.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiFacade.java
@@ -1,58 +1,52 @@
 package de.fau.amos.virtualledger.server.banking.adorsys.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import de.fau.amos.virtualledger.server.banking.model.BankAccessBankingModel;
 import de.fau.amos.virtualledger.server.banking.model.BankAccountBankingModel;
 import de.fau.amos.virtualledger.server.banking.model.BankingException;
 import de.fau.amos.virtualledger.server.banking.model.BookingModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
- * Class that can be used in other controllers to do interaction with banking api.
+ * Class that can be used in other controllers to do interaction with banking
+ * api.
  */
 @Component
 
 public class BankingApiFacade {
 
-
-    BankingApiBinder binder;
+    private BankingApiBinder binder;
 
     @Autowired
     public BankingApiFacade(BankingApiBinder binder) {
         this.binder = binder;
     }
+
     // protected empty constructor required for server to init it
-    protected BankingApiFacade() { }
+    protected BankingApiFacade() {
+    }
 
-
-
-
-    public void createUser(String userId)
-    {
+    public void createUser(String userId) {
         binder.getUserEndpoint(userId).createUser(userId);
     }
 
-    public List<BankAccessBankingModel> getBankAccesses(String userId) throws BankingException
-    {
+    public List<BankAccessBankingModel> getBankAccesses(String userId) throws BankingException {
         return binder.getBankAccessEndpoint(userId).getBankAccesses(userId);
     }
 
-    public List<BankAccountBankingModel> getBankAccounts(String userId, String bankAccessId) throws BankingException
-    {
+    public List<BankAccountBankingModel> getBankAccounts(String userId, String bankAccessId) throws BankingException {
         return binder.getBankAccountEndpoint(userId).getBankAccounts(userId, bankAccessId);
     }
 
-    public List<BookingModel> syncBankAccount(String userId, String bankAccessId, String bankAccountId, String pin) throws BankingException
-    {
+    public List<BookingModel> syncBankAccount(String userId, String bankAccessId, String bankAccountId, String pin)
+            throws BankingException {
         return binder.getBankAccountEndpoint(userId).syncBankAccount(userId, bankAccessId, bankAccountId, pin);
     }
 
-    public void addBankAccess(String userId, BankAccessBankingModel bankAccess) throws BankingException
-    {
+    public void addBankAccess(String userId, BankAccessBankingModel bankAccess) throws BankingException {
         binder.getBankAccessEndpoint(userId).addBankAccess(userId, bankAccess);
     }
 
