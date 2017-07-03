0a52d8d49d7f6d27d40c7c904f4174c17bc34c9d
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-03 20:33:49 +0200

    Generated ContactsEntity

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/ContactsEntity.java
@@ -0,0 +1,56 @@
+package de.fau.amos.virtualledger.server.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Contacts", schema = "amos5db")
+public class ContactsEntity {
+    private int id;
+    private String email;
+    private String firstname;
+    private String lastname;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "firstname")
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Basic
+    @Column(name = "lastname")
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
