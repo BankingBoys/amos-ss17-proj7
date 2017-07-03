5e8982eddda144ad286176411b923c42074a81d9
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-03 17:52:30 +0200

    Created contacts package with ContactsController

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
@@ -0,0 +1,12 @@
+package de.fau.amos.virtualledger.server.contacts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+public class ContactsController {
+    @SuppressWarnings("unused")
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+}
