79c3a6f9dab5541648df9ae6eaa2cac8b1169784
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-04 11:34:49 +0200

    Add method to ContactsController to get contacts by email

--- server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
@@ -1,5 +1,7 @@
 package de.fau.amos.virtualledger.server.contacts;
 
+import de.fau.amos.virtualledger.dtos.Contact;
+import de.fau.amos.virtualledger.server.model.ContactsEntity;
 import de.fau.amos.virtualledger.server.persistence.ContactsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Component
 public class ContactsController {
@@ -18,5 +23,10 @@ public class ContactsController {
     @Autowired
     public ContactsController(final ContactsRepository contactsRepository) {
         this.contactsRepository = contactsRepository;
+    }
+
+    public List<Contact> getContactsByEmail(final String email) {
+        final List<ContactsEntity> contactEntities = contactsRepository.getContactsByEmail(email);
+        return contactEntities.stream().map(entity -> new Contact(entity.getEmail(), entity.getFirstname(), entity.getLastname())).collect(toList());
     }
 }
