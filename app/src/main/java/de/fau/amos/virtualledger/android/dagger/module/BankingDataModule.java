0998d3de2fd3bc961edf3a429e6441130bd47574
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-07-04 18:31:24 +0200

    removed unused savings provider

--- app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/BankingDataModule.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/dagger/module/BankingDataModule.java
@@ -1,15 +1,12 @@
 package de.fau.amos.virtualledger.android.dagger.module;
 
-import javax.inject.Singleton;
-
 import dagger.Module;
 import dagger.Provides;
 import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
 import de.fau.amos.virtualledger.android.api.banking.BankingProvider;
-import de.fau.amos.virtualledger.android.api.savings.SavingsProvider;
 import de.fau.amos.virtualledger.android.dagger.component.NetComponentScope;
-import de.fau.amos.virtualledger.android.localStorage.BankAccessCredentialDB;
 import de.fau.amos.virtualledger.android.data.BankingDataManager;
+import de.fau.amos.virtualledger.android.localStorage.BankAccessCredentialDB;
 
 @Module(includes = {BankingModule.class, DatabaseModule.class, SavingsModule.class})
 public class BankingDataModule {
@@ -17,7 +14,7 @@ public class BankingDataModule {
 
     @Provides
     @NetComponentScope
-    BankingDataManager provideBankingDataManager(BankingProvider bankingProvider, SavingsProvider savingsProvider, BankAccessCredentialDB bankAccessCredentialDB, AuthenticationProvider authenticationProvider) {
-        return new BankingDataManager(bankingProvider, savingsProvider, bankAccessCredentialDB, authenticationProvider);
+    BankingDataManager provideBankingDataManager(BankingProvider bankingProvider, BankAccessCredentialDB bankAccessCredentialDB, AuthenticationProvider authenticationProvider) {
+        return new BankingDataManager(bankingProvider, bankAccessCredentialDB, authenticationProvider);
     }
 }
