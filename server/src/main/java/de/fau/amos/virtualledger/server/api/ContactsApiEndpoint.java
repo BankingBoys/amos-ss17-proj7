27e7a8e2165ee46eb22f2575505956cc0c8b8aa4
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-04 11:53:36 +0200

    Add getContacts Endpoint

--- server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
@@ -1,9 +1,16 @@
 package de.fau.amos.virtualledger.server.api;
 
 import de.fau.amos.virtualledger.server.contacts.ContactsController;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
@@ -21,6 +28,23 @@ public class ContactsApiEndpoint {
     @Autowired
     public ContactsApiEndpoint(final ContactsController contactsController) {
         this.contactsController = contactsController;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "api/contacts", produces = "application/json")
+    public ResponseEntity<?> getContactsEndpoint() {
+        final KeycloakPrincipal principal = (KeycloakPrincipal<KeycloakSecurityContext>) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        final String username = principal.getKeycloakSecurityContext().getToken().getEmail();
+
+        if (username == null || username.isEmpty()) {
+            return new ResponseEntity<>("Authentication failed! Your username wasn't found.", HttpStatus.FORBIDDEN);
+        }
+        LOGGER.info("getContactsEndpoint of " + username + " was requested");
+
+        return this.getContacts(username);
+    }
+
+    private ResponseEntity<?> getContacts(final String username) {
+        return new ResponseEntity<>(contactsController.getContactsByEmail(username), HttpStatus.OK);
     }
 
 }
