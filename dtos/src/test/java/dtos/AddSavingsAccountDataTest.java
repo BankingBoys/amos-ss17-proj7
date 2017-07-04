e8fa9e9cf73b3da56e5eab8d69e73b5745163f07
simonschmoll <simon.schmoll@gmail.com>
2017-07-04 23:35:42 +0200

    added test for AddSavingsAccountData with setUp method

---
+++ dtos/src/test/java/dtos/AddSavingsAccountDataTest.java
@@ -0,0 +1,24 @@
+package dtos;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import de.fau.amos.virtualledger.dtos.AddSavingsAccountData;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by Simon on 04.07.2017.
+ */
+
+public class AddSavingsAccountDataTest {
+
+    private AddSavingsAccountData componentUnderTest;
+
+    @Before
+    public void setUp() {
+        componentUnderTest = new AddSavingsAccountData();
+    }
+    
+}
