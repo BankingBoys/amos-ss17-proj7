4d69b8b6f264ca886547a6f353e189e3be3c343e
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-04 11:26:04 +0200

    Inject ContactsRepository in ContactsController

--- server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
@@ -1,12 +1,22 @@
 package de.fau.amos.virtualledger.server.contacts;
 
+import de.fau.amos.virtualledger.server.persistence.ContactsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
 
+@Component
 public class ContactsController {
     @SuppressWarnings("unused")
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private final ContactsRepository contactsRepository;
+
+    @Autowired
+    public ContactsController(final ContactsRepository contactsRepository) {
+        this.contactsRepository = contactsRepository;
+    }
 }
