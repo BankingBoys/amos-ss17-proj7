5ebe50fc542cbfc0dae92576b5b734409da2adfa
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-04 11:48:31 +0200

    Inject ContactsController in ContactsApiEndpoint

--- server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
@@ -1,5 +1,6 @@
 package de.fau.amos.virtualledger.server.api;
 
+import de.fau.amos.virtualledger.server.contacts.ContactsController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,11 @@ public class ContactsApiEndpoint {
     @SuppressWarnings("unused")
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private final ContactsController contactsController;
 
     @Autowired
-    public ContactsApiEndpoint() {
-
+    public ContactsApiEndpoint(final ContactsController contactsController) {
+        this.contactsController = contactsController;
     }
 
 }
