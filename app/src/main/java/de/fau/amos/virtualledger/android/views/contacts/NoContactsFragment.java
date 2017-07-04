7733c21661369ca64e680065922c663ed83863ca
simonschmoll <simon.schmoll@gmail.com>
2017-07-04 20:32:51 +0200

    added new string ressources as preperation for the no contacts screen

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/contacts/NoContactsFragment.java
@@ -0,0 +1,10 @@
+package de.fau.amos.virtualledger.android.views.contacts;
+
+import android.app.Fragment;
+
+/**
+ * Created by Simon on 04.07.2017.
+ */
+
+public class NoContactsFragment extends Fragment {
+}
