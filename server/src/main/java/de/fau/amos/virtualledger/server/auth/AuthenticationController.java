89e9080c383459ecb12aa32e3f5ff563325f72ad
Georg <georg.schwarz@fau.de>
2017-07-05 16:07:53 +0200

    deleted unused method from AuthenticationController

--- server/src/main/java/de/fau/amos/virtualledger/server/auth/AuthenticationController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/auth/AuthenticationController.java
@@ -1,29 +1,24 @@
 package de.fau.amos.virtualledger.server.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import de.fau.amos.virtualledger.dtos.LoginData;
-import de.fau.amos.virtualledger.dtos.SessionData;
-import de.fau.amos.virtualledger.server.banking.adorsys.api.BankingApiFacade;
 import de.fau.amos.virtualledger.server.model.UserCredential;
 import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Controller / Service class for authentication
  */
 @Component
-
 public class AuthenticationController {
 
-    @Autowired
     private UserCredentialRepository userCredentialRepository;
 
     @Autowired
-    private SessionIdGenerator sessionIdGenerator;
-
-    @Autowired
-    private BankingApiFacade bankingApiFacade;
+    public AuthenticationController(UserCredentialRepository userCredentialRepository) {
+        this.userCredentialRepository = userCredentialRepository;
+    }
+    protected AuthenticationController() {
+    }
 
     /**
      * register a new user, if attributes are null or don't follow the specific
@@ -35,68 +30,15 @@ public class AuthenticationController {
      */
     public String register(UserCredential credential) throws VirtualLedgerAuthenticationException {
         if (credential == null || credential.getEmail() == null || credential.getPassword() == null
-                || credential.getFirstname() == null || credential.getLastname() == null) { // if
-                                                                                            // not
-                                                                                            // null,
-                                                                                            // matches
-                                                                                            // the
-                                                                                            // pattern
-                                                                                            // ->
-                                                                                            // specified
-                                                                                            // in
-                                                                                            // model
-                                                                                            // class
+                || credential.getFirstname() == null || credential.getLastname() == null) {
             throw new VirtualLedgerAuthenticationException(
                     "Please check your inserts! At least one was not formatted correctly!");
         }
-        if (this.getUserCredentialRepository().existsUserCredentialEmail(credential.getEmail())) {
+        if (this.userCredentialRepository.existsUserCredentialEmail(credential.getEmail())) {
             throw new VirtualLedgerAuthenticationException("There already exists an account with this Email address.");
         }
-        this.getBankingApiFacade().createUser(credential.getEmail());
-        this.getUserCredentialRepository().createUserCredential(credential);
+        this.userCredentialRepository.createUserCredential(credential);
 
         return "You were registered! " + credential.getEmail();
-    }
-
-    public SessionData login(final LoginData loginData) throws InvalidCredentialsException {
-        final boolean valid = getUserCredentialRepository().checkLogin(loginData);
-        if (!valid) {
-            throw new InvalidCredentialsException();
-        }
-        final String sessionId = getSessionIdGenerator().generate();
-        getUserCredentialRepository().persistSessionId(loginData.getEmail(), sessionId);
-
-        final SessionData result = new SessionData();
-        result.setEmail(loginData.getEmail());
-        result.setSessionid(sessionId);
-        return result;
-    }
-
-    public void logout(final String email) {
-        getUserCredentialRepository().deleteSessionIdsByEmail(email);
-    }
-
-    public UserCredentialRepository getUserCredentialRepository() {
-        return userCredentialRepository;
-    }
-
-    public void setUserCredentialRepository(UserCredentialRepository userCredentialRepository) {
-        this.userCredentialRepository = userCredentialRepository;
-    }
-
-    public SessionIdGenerator getSessionIdGenerator() {
-        return sessionIdGenerator;
-    }
-
-    public void setSessionIdGenerator(SessionIdGenerator sessionIdGenerator) {
-        this.sessionIdGenerator = sessionIdGenerator;
-    }
-
-    public BankingApiFacade getBankingApiFacade() {
-        return bankingApiFacade;
-    }
-
-    public void setBankingApiFacade(BankingApiFacade bankingApiFacade) {
-        this.bankingApiFacade = bankingApiFacade;
     }
 }
--- server/src/test/java/de/fau/amos/virtualledger/server/auth/AuthenticationControllerTest.java
+++ server/src/test/java/de/fau/amos/virtualledger/server/auth/AuthenticationControllerTest.java
@@ -1,27 +1,22 @@
 package de.fau.amos.virtualledger.server.auth;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.junit.Test;
-
-import de.fau.amos.virtualledger.dtos.LoginData;
-import de.fau.amos.virtualledger.dtos.SessionData;
-import de.fau.amos.virtualledger.server.banking.adorsys.api.BankingApiFacade;
 import de.fau.amos.virtualledger.server.model.UserCredential;
 import de.fau.amos.virtualledger.server.persistence.UserCredentialRepository;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationControllerTest {
     @Test(expected = VirtualLedgerAuthenticationException.class)
     public void testRegisterWithExistingUserShouldThrowException() throws Exception {
         // Arrange
-        AuthenticationController componentUnderTest = new AuthenticationController();
 
         UserCredentialRepository userCredentialRepositoryMock = mock(UserCredentialRepository.class);
         when(userCredentialRepositoryMock.existsUserCredentialEmail(someEmail())).thenReturn(Boolean.TRUE);
-        componentUnderTest.setUserCredentialRepository(userCredentialRepositoryMock);
+
+        AuthenticationController componentUnderTest = new AuthenticationController(userCredentialRepositoryMock);
 
         UserCredential credential = getValidUserCredential();
         credential.setEmail(someEmail());
@@ -33,64 +28,16 @@ public class AuthenticationControllerTest {
     @Test
     public void testRegisterWithValidUserShouldRegister() throws Exception {
         // Arrange
-        AuthenticationController componentUnderTest = new AuthenticationController();
 
         UserCredentialRepository userCredentialRepositoryMock = mock(UserCredentialRepository.class);
-        componentUnderTest.setUserCredentialRepository(userCredentialRepositoryMock);
 
-        BankingApiFacade bankingFacadeMock = mock(BankingApiFacade.class);
-        componentUnderTest.setBankingApiFacade(bankingFacadeMock);
+        AuthenticationController componentUnderTest = new AuthenticationController(userCredentialRepositoryMock);
 
         // Act
         String result = componentUnderTest.register(getValidUserCredential());
 
         // Assert
         assertThat(result).isEqualTo("You were registered! blablabl@bbla.de");
-        verify(bankingFacadeMock).createUser(getValidUserCredential().getEmail());
-    }
-
-    @Test
-    public void testLoginWithValidLoginDataShouldLogIn() throws Exception {
-        // Arrange
-        AuthenticationController componentUnderTest = new AuthenticationController();
-
-        LoginData validLoginData = validLoginData();
-        UserCredentialRepository userCredentialRepositoryMock = mock(UserCredentialRepository.class);
-        when(userCredentialRepositoryMock.checkLogin(validLoginData)).thenReturn(Boolean.TRUE);
-        componentUnderTest.setUserCredentialRepository(userCredentialRepositoryMock);
-
-        SessionIdGenerator generatorMock = mock(SessionIdGenerator.class);
-        when(generatorMock.generate()).thenReturn(expectedSessionID());
-        componentUnderTest.setSessionIdGenerator(generatorMock);
-
-        // Act
-        SessionData result = componentUnderTest.login(validLoginData);
-
-        // Assert
-        assertThat(result.getEmail()).isEqualTo(validLoginData.getEmail());
-        assertThat(result.getSessionid()).isEqualTo(expectedSessionID());
-    }
-
-    @Test(expected = InvalidCredentialsException.class)
-    public void testLoginWithInvalidLoginDataShouldThrowException() throws Exception {
-        // Arrange
-        AuthenticationController componentUnderTest = new AuthenticationController();
-
-        LoginData validLoginData = validLoginData();
-        UserCredentialRepository userCredentialRepositoryMock = mock(UserCredentialRepository.class);
-        when(userCredentialRepositoryMock.checkLogin(validLoginData)).thenReturn(Boolean.FALSE);
-        componentUnderTest.setUserCredentialRepository(userCredentialRepositoryMock);
-
-        // Act
-        componentUnderTest.login(validLoginData);
-    }
-
-    private String expectedSessionID() {
-        return "some session id";
-    }
-
-    private LoginData validLoginData() {
-        return new LoginData("some@mail.de", "fancy fancy");
     }
 
     private UserCredential getValidUserCredential() {
