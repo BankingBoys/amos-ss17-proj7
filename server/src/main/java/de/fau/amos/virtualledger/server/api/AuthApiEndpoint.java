9e870228cde37d5d91db598e406eefb71f6766ad
Georg <georg.schwarz@fau.de>
2017-07-05 15:53:09 +0200

    unimplemented register method (because not used by now)

--- server/src/main/java/de/fau/amos/virtualledger/server/api/AuthApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/AuthApiEndpoint.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.lang.invoke.MethodHandles;
 
@@ -59,8 +60,8 @@ public class AuthApiEndpoint {
                     HttpStatus.BAD_REQUEST);
         }
         LOGGER.info("Registration for " + credential.getEmail() + " was requested.");
-
-        return this.register(credential);
+        throw new NotImplementedException();
+        //return this.register(credential);
     }
 
     /**
