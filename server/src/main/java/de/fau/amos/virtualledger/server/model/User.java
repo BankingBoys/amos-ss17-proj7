8772394baab2634c008f792c4513fcd42531f9fc
Georg <georg.schwarz@fau.de>
2017-07-05 15:09:19 +0200

    created class User that will interact with database in future

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/User.java
@@ -0,0 +1,41 @@
+package de.fau.amos.virtualledger.server.model;
+
+public class User {
+
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    public User(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
