fe3529cffdcd126a6b53bfe3196663a613133cd3
benestr <strassnerbenedikt@gmail.com>
2017-07-07 15:55:14 +0200

    Run server on aws after deploy

--- tools/upload_script_to_amazon_aws.sh
+++ tools/upload_script_to_amazon_aws.sh
@@ -12,4 +12,6 @@ cd build
 eval "$(ssh-agent -s)"
 chmod 400 /home/travis/build/BankingBoys/amos-ss17-proj7/keyssh
 ssh-add /home/travis/build/BankingBoys/amos-ss17-proj7/keyssh
+ssh -i /home/travis/build/BankingBoys/amos-ss17-proj7/keyssh ubuntu@$AWS_SERVER "killall java"
 scp -rp -i /home/travis/build/BankingBoys/amos-ss17-proj7/keyssh /home/travis/build/BankingBoys/amos-ss17-proj7/server/build/libs/server-1.0-SNAPSHOT.jar ubuntu@$AWS_SERVER:/home/ubuntu/
+ssh -i /home/travis/build/BankingBoys/amos-ss17-proj7/keyssh ubuntu@$AWS_SERVER "java -jar /home/ubuntu/server-1.0-SNAPSHOT.jar > /home/ubuntu/log &"
