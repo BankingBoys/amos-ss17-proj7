75873d9bdc27c6e0b7989a8983f739e4fb930523
Georg <georg.schwarz@fau.de>
2017-07-07 11:09:37 +0200

    refactored ContactsApiEndpoint to use keycloakUtilizer

--- server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
@@ -1,15 +1,13 @@
 package de.fau.amos.virtualledger.server.api;
 
 import de.fau.amos.virtualledger.dtos.Contact;
+import de.fau.amos.virtualledger.server.auth.KeycloakUtilizer;
 import de.fau.amos.virtualledger.server.contacts.ContactsController;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,17 +23,18 @@ public class ContactsApiEndpoint {
     @SuppressWarnings("unused")
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private KeycloakUtilizer keycloakUtilizer;
     private final ContactsController contactsController;
 
     @Autowired
-    public ContactsApiEndpoint(final ContactsController contactsController) {
+    public ContactsApiEndpoint(KeycloakUtilizer keycloakUtilizer, final ContactsController contactsController) {
+        this.keycloakUtilizer = keycloakUtilizer;
         this.contactsController = contactsController;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "api/contacts", produces = "application/json")
     public ResponseEntity<?> getContactsEndpoint() {
-        final KeycloakPrincipal principal = (KeycloakPrincipal<KeycloakSecurityContext>) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        final String username = principal.getKeycloakSecurityContext().getToken().getEmail();
+        final String username = keycloakUtilizer.getEmail();
 
         if (username == null || username.isEmpty()) {
             return new ResponseEntity<>("Authentication failed! Your username wasn't found.", HttpStatus.FORBIDDEN);
@@ -47,8 +46,7 @@ public class ContactsApiEndpoint {
 
     @RequestMapping(method = RequestMethod.POST, value = "api/contacts", produces = "application/json")
     public ResponseEntity<?> addContactEndpoint(@RequestBody final Contact contact) {
-        final KeycloakPrincipal principal = (KeycloakPrincipal<KeycloakSecurityContext>) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        final String username = principal.getKeycloakSecurityContext().getToken().getEmail();
+        final String username = keycloakUtilizer.getEmail();
 
         if (username == null || username.isEmpty()) {
             return new ResponseEntity<>("Authentication failed! Your username wasn't found.", HttpStatus.FORBIDDEN);
