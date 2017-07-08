02816bc08a8020fb5a3ee98f62f02a2749c69211
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-08 15:40:28 +0200

    Fix warnings in HttpBankAccessEndpoint.java

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/bankAccessEndpoint/HttpBankAccessEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/bankAccessEndpoint/HttpBankAccessEndpoint.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
@@ -39,38 +38,37 @@ public class HttpBankAccessEndpoint implements BankAccessEndpoint {
     }
 
     @Override
-    public List<BankAccessBankingModel> getBankAccesses(final String userId) throws BankingException {
+    public List<BankAccessBankingModel> getBankAccesses(final String token) throws BankingException {
 
         // Create Jersey client
         final Client client = JerseyClientUtility.getLoggingClient(LOGGER);
 
-        String url = urlProvider.getBankAccessEndpointUrl();
-        WebTarget webTarget = client.target(url);
-        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
-        Response response = invocationBuilder.get();
+        final String url = urlProvider.getBankAccessEndpointUrl();
+        final WebTarget webTarget = client.target(url);
+        final Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE).header("Authorization", token);
+        final Response response = invocationBuilder.get();
 
         if (response.getStatus() != HTTP_OK) {
             throw new BankingException("No connection to Adorsys Server!");
         }
-        BankAccessJSONBankingModel reponseModel = response.readEntity(BankAccessJSONBankingModel.class);
-        if (reponseModel == null || reponseModel.getEmbedded() == null) {
+        final BankAccessJSONBankingModel responseModel = response.readEntity(BankAccessJSONBankingModel.class);
+        if (responseModel == null || responseModel.getEmbedded() == null) {
             LOGGER.info("No access found!");
-            return new ArrayList<BankAccessBankingModel>();
+            return new ArrayList<>();
         }
-        List<BankAccessBankingModel> result = reponseModel.getEmbedded().getBankAccessEntityList();
-        return result;
+        return responseModel.getEmbedded().getBankAccessEntityList();
     }
 
     @Override
-    public void addBankAccess(String userId, BankAccessBankingModel bankAccess) throws BankingException {
+    public void addBankAccess(final String userId, final BankAccessBankingModel bankAccess) throws BankingException {
 
         // Create Jersey client
-        Client client = ClientBuilder.newClient();
+        final Client client = JerseyClientUtility.getLoggingClient(LOGGER);
 
-        String url = urlProvider.getBankAccessEndpointUrl();
-        WebTarget webTarget = client.target(url);
-        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
-        Response response = invocationBuilder.post(Entity.entity(bankAccess, MediaType.APPLICATION_JSON_TYPE));
+        final String url = urlProvider.getBankAccessEndpointUrl();
+        final WebTarget webTarget = client.target(url);
+        final Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
+        final Response response = invocationBuilder.post(Entity.entity(bankAccess, MediaType.APPLICATION_JSON_TYPE));
 
         if (response.getStatus() != HTTP_OK_RESPONSE) {
             throw new BankingException("Creating Banking Access failed!");
