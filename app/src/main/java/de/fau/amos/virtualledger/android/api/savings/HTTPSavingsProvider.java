863d7c751310ce84a6441adebd36431065e9782e
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-08 12:19:48 +0200

    Use RetrofitCallback in HTTPSavingsProvider.java methods

--- app/src/main/java/de/fau/amos/virtualledger/android/api/savings/HTTPSavingsProvider.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/savings/HTTPSavingsProvider.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import de.fau.amos.virtualledger.android.api.RestApi;
 import de.fau.amos.virtualledger.android.api.auth.AuthenticationProvider;
+import de.fau.amos.virtualledger.android.api.shared.RetrofitCallback;
 import de.fau.amos.virtualledger.android.model.SavingsAccount;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -14,8 +15,6 @@ import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class HTTPSavingsProvider implements SavingsProvider {
 
@@ -42,30 +41,7 @@ public class HTTPSavingsProvider implements SavingsProvider {
                     @Override
                     public void accept(@NonNull final String token) throws Exception {
                         // got token
-                        final retrofit2.Call<List<SavingsAccount>> responseMessage = restApi.getSavingAccounts(token);
-
-                        responseMessage.enqueue(new Callback<List<SavingsAccount>>() {
-                            @Override
-                            public void onResponse(retrofit2.Call<List<SavingsAccount>> call, Response<List<SavingsAccount>> response) {
-                                // got response from server
-                                if (response.isSuccessful()) {
-                                    List<SavingsAccount> savingsAccounts = response.body();
-                                    Log.v(TAG, "Fetching of saving accounts was successful " + response.code());
-                                    observable.onNext(savingsAccounts);
-                                } else {
-                                    Log.e(TAG, "Fetchin of saving accounts was not successful! ERROR " + response.code());
-                                    observable.onError(new Throwable("Fetching of saving accounts was not successful!"));
-                                }
-                            }
-
-
-                            @Override
-                            public void onFailure(retrofit2.Call<List<SavingsAccount>> call, Throwable t) {
-                                // reponse from server failed
-                                Log.e(TAG, "No connection to server!");
-                                observable.onError(new Throwable("No connection to server!"));
-                            }
-                        });
+                        restApi.getSavingAccounts(token).enqueue(new RetrofitCallback<>(observable));
                     }
                 }, new Consumer<Throwable>() {
                     @Override
@@ -91,28 +67,7 @@ public class HTTPSavingsProvider implements SavingsProvider {
                     @Override
                     public void accept(@NonNull final String token) throws Exception {
                         // got token
-                        final retrofit2.Call<Void> responseMessage = restApi.addSavingAccounts(token, savingsAccount);
-
-                        responseMessage.enqueue(new Callback<Void>() {
-                            @Override
-                            public void onResponse(retrofit2.Call<Void> call, Response<Void> response) {
-                                // got resoinse from server
-                                if (response.code() == 201) {
-                                    Log.v(TAG, "Adding Saving Accounts was successful " + response.code());
-                                    observable.onNext(response.body());
-                                } else {
-                                    Log.e(TAG, "Adding Saving Accounts was not successful! ERROR " + response.code());
-                                    observable.onError(new Throwable("Adding Saving Accounts was not successful!"));
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(retrofit2.Call<Void> call, Throwable t) {
-                                // response from server failed
-                                Log.e(TAG, "No connection to server!");
-                                observable.onError(new Throwable("No connection to server!"));
-                            }
-                        });
+                        restApi.addSavingAccounts(token, savingsAccount).enqueue(new RetrofitCallback<>(observable));
                     }
                 }, new Consumer<Throwable>() {
                     @Override
