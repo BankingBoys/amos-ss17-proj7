7eae6f89e14807f9afa74cb4ecdae41f41f85c9b
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-08 15:04:39 +0200

    Simplified KeycloakUtilizer.java, throws unnecessarily caught exceptions

--- server/src/main/java/de/fau/amos/virtualledger/server/auth/KeycloakUtilizer.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/auth/KeycloakUtilizer.java
@@ -15,42 +15,24 @@ public class KeycloakUtilizer {
      * extracts the email adress from the keycloak context.
      * returns null if not successful.
      */
-    public String getEmail() {
-        final AccessToken token;
-        try {
-            token = getAccessToken();
-        } catch (final ServletException e) {
-            return null;
-        }
-        return token.getEmail();
+    public String getEmail() throws ServletException {
+        return getAccessToken().getEmail();
     }
 
     /**
      * extracts the first name from the keycloak context.
      * returns null if not successful.
      */
-    public String getFirstName() {
-        final AccessToken token;
-        try {
-            token = getAccessToken();
-        } catch (final ServletException e) {
-            return null;
-        }
-        return token.getGivenName();
+    public String getFirstName() throws ServletException {
+        return getAccessToken().getGivenName();
     }
 
     /**
      * extracts the last name from the keycloak context.
      * returns null if not successful.
      */
-    public String getLastName() {
-        final AccessToken token;
-        try {
-            token = getAccessToken();
-        } catch (final ServletException e) {
-            return null;
-        }
-        return token.getFamilyName();
+    public String getLastName() throws ServletException {
+        return getAccessToken().getFamilyName();
     }
 
     public String getTokenString() {
@@ -59,13 +41,11 @@ public class KeycloakUtilizer {
     }
 
     private AccessToken getAccessToken() throws ServletException {
-        final AccessToken token;
         try {
             @SuppressWarnings("unchecked") final KeycloakPrincipal principal = (KeycloakPrincipal<KeycloakSecurityContext>) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            token = principal.getKeycloakSecurityContext().getToken();
+            return principal.getKeycloakSecurityContext().getToken();
         } catch (final Exception ex) {
             throw new ServletException("Failure at getting data about the user by the identity token!");
         }
-        return token;
     }
 }
