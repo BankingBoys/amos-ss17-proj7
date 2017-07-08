788c6783fb8b1aed8244b6e3ca50dee6554893c3
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-08 14:22:49 +0200

    Clean up BankingApiFacade.java

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiFacade.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiFacade.java
@@ -20,7 +20,7 @@ public class BankingApiFacade {
     private BankingApiBinder binder;
 
     @Autowired
-    public BankingApiFacade(BankingApiBinder binder) {
+    public BankingApiFacade(final BankingApiBinder binder) {
         this.binder = binder;
     }
 
@@ -28,20 +28,20 @@ public class BankingApiFacade {
     protected BankingApiFacade() {
     }
 
-    public List<BankAccessBankingModel> getBankAccesses(String userId) throws BankingException {
+    public List<BankAccessBankingModel> getBankAccesses(final String userId) throws BankingException {
         return binder.getBankAccessEndpoint(userId).getBankAccesses(userId);
     }
 
-    public List<BankAccountBankingModel> getBankAccounts(String userId, String bankAccessId) throws BankingException {
+    public List<BankAccountBankingModel> getBankAccounts(final String userId, final String bankAccessId) throws BankingException {
         return binder.getBankAccountEndpoint(userId).getBankAccounts(userId, bankAccessId);
     }
 
-    public List<BookingModel> syncBankAccount(String userId, String bankAccessId, String bankAccountId, String pin)
+    public List<BookingModel> syncBankAccount(final String userId, final String bankAccessId, final String bankAccountId, final String pin)
             throws BankingException {
         return binder.getBankAccountEndpoint(userId).syncBankAccount(userId, bankAccessId, bankAccountId, pin);
     }
 
-    public void addBankAccess(String userId, BankAccessBankingModel bankAccess) throws BankingException {
+    public void addBankAccess(final String userId, final BankAccessBankingModel bankAccess) throws BankingException {
         binder.getBankAccessEndpoint(userId).addBankAccess(userId, bankAccess);
     }
 
