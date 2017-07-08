a36c633b78f335cbc7f8d75496a9974c1bd9ec0d
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-08 11:21:32 +0200

    Added general Callback to avoid duplicate code

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/shared/RetrofitCallback.java
@@ -0,0 +1,40 @@
+package de.fau.amos.virtualledger.android.api.shared;
+
+import android.util.Log;
+
+import io.reactivex.subjects.PublishSubject;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RetrofitCallback<T> implements Callback<T> {
+
+    private static final String TAG = RetrofitCallback.class.getSimpleName();
+
+    final PublishSubject observable;
+
+    public RetrofitCallback(final PublishSubject observable) {
+        this.observable = observable;
+    }
+
+    @Override
+    public void onResponse(final Call<T> call, final Response<T> response) {
+        // got response from server
+        final String requestString = "Request " + call.request().method() + " " + call.request().url() + " ";
+        if (response.isSuccessful()) {
+            T result = response.body();
+            Log.v(TAG, requestString + "was successful: " + response.code());
+            observable.onNext(result);
+        } else {
+            Log.e(TAG, requestString + "was not successful! ERROR " + response.code());
+            observable.onError(new Throwable(requestString + " was not successful!"));
+        }
+    }
+
+    @Override
+    public void onFailure(final Call<T> call, final Throwable t) {
+        // response of server failed
+        Log.e(TAG, "No connection to server!");
+        observable.onError(new Throwable("No connection to server!"));
+    }
+}
