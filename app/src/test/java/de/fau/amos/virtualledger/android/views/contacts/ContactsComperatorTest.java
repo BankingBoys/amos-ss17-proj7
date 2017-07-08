e2b54d0696011bf994a6dc0577fbfdbf95b78d60
simonschmoll <simon.schmoll@gmail.com>
2017-07-08 16:55:24 +0200

    new test method for ignoring the case while comparing contacts

--- app/src/test/java/de/fau/amos/virtualledger/android/views/contacts/ContactsComperatorTest.java
+++ app/src/test/java/de/fau/amos/virtualledger/android/views/contacts/ContactsComperatorTest.java
@@ -64,4 +64,21 @@ public class ContactsComperatorTest {
         assertThat(component_under_test.compare(contact1, contact2)).isGreaterThan(referencedValue);
     }
 
+    @Test
+    public void testIgnoringCases() {
+        ContactsComparator component_under_test = new ContactsComparator();
+        Contact contact1 = new Contact();
+        Contact contact2 = new Contact();
+        final String testFirstName = "Gundula";
+        final String testLastName = "Gause";
+        final String testFirstName2 = "gundula";
+        final String testLastName2 = "gause";
+        contact1.setFirstName(testFirstName);
+        contact1.setLastName(testLastName);
+        contact2.setFirstName(testFirstName2);
+        contact2.setLastName(testLastName2);
+        final int expectedValue = 0;
+
+        assertThat(component_under_test.compare(contact1, contact2)).isEqualTo(expectedValue);
+    }
 }
