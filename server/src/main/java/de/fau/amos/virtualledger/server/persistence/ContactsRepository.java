b4b72318221062417c1137ef5300dcd0838dd685
simonschmoll <simon.schmoll@gmail.com>
2017-07-10 12:37:03 +0200

    new queries for getting all Contacts and if contact already exists

--- server/src/main/java/de/fau/amos/virtualledger/server/persistence/ContactsRepository.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/persistence/ContactsRepository.java
@@ -1,31 +1,20 @@
 package de.fau.amos.virtualledger.server.persistence;
 
-import de.fau.amos.virtualledger.dtos.Contact;
 import de.fau.amos.virtualledger.server.model.ContactsEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import de.fau.amos.virtualledger.server.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class ContactsRepository {
-    @SuppressWarnings("unused")
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+@Repository
+public interface ContactsRepository extends CrudRepository<ContactsEntity, Integer> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Query(value = "select case when count(c) > 0 then true else false end from ContactsEntity c where c.owner=:owner AND c.contact=:contact")
+    boolean existsContactwithEmail(@Param("owner") User owner, @Param("contact") User contact);
 
-
-    public void createContact(final Contact contact, final String userEmail) {
-
-    }
-
-    public List<ContactsEntity> getContactsByEmail(final String email) {
-        return new ArrayList<ContactsEntity>();
-    }
+    @Query("SELECT c FROM ContactsEntity c LEFT JOIN FETCH c.contacts AS c WHERE c.owner = (:owner)")
+    List<ContactsEntity> findAllByOwner(@Param("owner") User owner);
 }
