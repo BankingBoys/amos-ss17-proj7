c12a08b8e1bc030a2f2a97f0fb49dda9436c5ae5
simonschmoll <simon.schmoll@gmail.com>
2017-07-10 11:14:24 +0200

    changes in COntactsEntity, now owner is mapped to contact

--- server/src/main/java/de/fau/amos/virtualledger/server/model/ContactsEntity.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/ContactsEntity.java
@@ -4,16 +4,20 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Contacts", schema = "amos5db")
 public class ContactsEntity {
     private int id;
-    private String email;
-    private String firstname;
-    private String lastname;
-    private Integer userId;
+
+    @OneToOne
+    private User owner;
+
+    @OneToOne
+    private User contact;
+
 
     @Id
     @Column(name = "id")
@@ -26,43 +30,22 @@ public class ContactsEntity {
     }
 
     @Basic
-    @Column(name = "email")
-    public String getEmail() {
-        return email;
+    @Column(name = "owner")
+    public User getOwner() {
+        return this.owner;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Basic
-    @Column(name = "firstname")
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void setOwner(User owner) {
+        this.owner = owner;
     }
 
     @Basic
-    @Column(name = "lastname")
-    public String getLastname() {
-        return lastname;
+    @Column(name = "contact")
+    public User getContact() {
+        return this.contact;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setContact(User contact) {
+        this.contact = contact;
     }
-
-    @Basic
-    @Column(name = "userId")
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
 }
