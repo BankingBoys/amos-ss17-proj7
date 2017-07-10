87b8f7d96c789fe87f87058c5fc46f01828ca472
simonschmoll <simon.schmoll@gmail.com>
2017-07-10 12:34:15 +0200

    checkstyle: removed .* import

--- server/src/main/java/de/fau/amos/virtualledger/server/model/ContactsEntity.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/model/ContactsEntity.java
@@ -1,6 +1,14 @@
 package de.fau.amos.virtualledger.server.model;
 
-import javax.persistence.*;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "contacts")
