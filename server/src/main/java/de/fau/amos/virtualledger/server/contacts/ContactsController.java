dfd07872656d338ffd7ab743f5d33f1ed4e111ed
simonschmoll <simon.schmoll@gmail.com>
2017-07-10 12:38:05 +0200

    implementation for getting Contacts by Email address

--- server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/contacts/ContactsController.java
@@ -1,10 +1,14 @@
 package de.fau.amos.virtualledger.server.contacts;
 
 import de.fau.amos.virtualledger.dtos.Contact;
+import de.fau.amos.virtualledger.server.model.ContactsEntity;
+import de.fau.amos.virtualledger.server.model.User;
 import de.fau.amos.virtualledger.server.persistence.ContactsRepository;
+import de.fau.amos.virtualledger.server.persistence.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
@@ -17,17 +21,34 @@ public class ContactsController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final ContactsRepository contactsRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ContactsController(final ContactsRepository contactsRepository) {
+    public ContactsController(final ContactsRepository contactsRepository, final UserRepository userRepository) {
         this.contactsRepository = contactsRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<Contact> getContactsByEmail(final String email) {
-        return new ArrayList<Contact>();
+    public List<Contact> getContactsByEmail(final String email) throws UserNotFoundException {
+        User user = userRepository.findOne(email);
+        if (user == null) {
+            throw new UserNotFoundException("User could not be found");
+        }
+        List<ContactsEntity> contacts = contactsRepository.findAllByOwner(user);
+        return createContactFromEntities(contacts);
+    }
+
+    private List<Contact> createContactFromEntities(List<ContactsEntity> contactsEntity) {
+        List<Contact> contactList = new ArrayList<>();
+        for (ContactsEntity c : contactsEntity) {
+            Contact contact = new Contact(c.getContact().getEmail(), c.getContact().getFirstName(),
+                    c.getContact().getLastName());
+            contactList.add(contact);
+        }
+        return contactList;
     }
 
     public void addContact(final Contact contact, final String userEmail) {
-        contactsRepository.createContact(contact, userEmail);
+
     }
 }
