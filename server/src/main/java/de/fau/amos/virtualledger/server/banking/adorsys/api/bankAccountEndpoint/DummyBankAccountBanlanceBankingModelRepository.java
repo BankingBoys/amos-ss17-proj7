ab5d3a0bd288b534b4bf3e69d8c7d04f6c0e17f6
Georg <georg.schwarz@fau.de>
2017-07-14 17:54:50 +0200

    deleted DummyBankAccountBanlanceBankingModelRepository.java because it became obsolete

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/bankAccountEndpoint/DummyBankAccountBanlanceBankingModelRepository.java
+++
@@ -1,8 +0,0 @@
-package de.fau.amos.virtualledger.server.banking.adorsys.api.bankAccountEndpoint;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DummyBankAccountBanlanceBankingModelRepository extends JpaRepository<DummyBankAccountBalanceBankingModelEntity, Integer> {
-}
