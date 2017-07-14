195213f844a79914463c399eb095a8e404b64df4
Georg <georg.schwarz@fau.de>
2017-07-14 17:03:27 +0200

    added repository for banking accounts

---
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/bankAccountEndpoint/DummyBankAccountBankingModelRepository.java
@@ -0,0 +1,17 @@
+package de.fau.amos.virtualledger.server.banking.adorsys.api.bankAccountEndpoint;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface DummyBankAccountBankingModelRepository extends JpaRepository<DummyBankAccountBankingModelEntity, String> {
+
+    @Query("select case when count(account) > 0 then true else false end from DummyBankAccountBankingModelEntity account left join account.bankAccess as access where access.id=:accessId")
+    boolean existBankAccountsForAccessId(@Param("accessId") String accessId);
+
+
+    @Query("SELECT a FROM DummyBankAccountBankingModelEntity a LEFT JOIN FETCH a.bankAccountBalance AS b WHERE a.bankAccess.id=:accessId")
+    List<DummyBankAccountBankingModelEntity> findAllByAccessIdAndLoadBalances(@Param("accessId") String accessId);
+}
