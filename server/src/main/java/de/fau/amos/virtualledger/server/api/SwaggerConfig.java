ae40db142086bbc9b03179b126ae16b0b03344e4
Georg <georg.schwarz@fau.de>
2017-07-15 14:30:56 +0200

    added some additional configuration to the swagger configuration (waiting on adorsys answer to solve issue with authentication)

--- server/src/main/java/de/fau/amos/virtualledger/server/api/SwaggerConfig.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/SwaggerConfig.java
@@ -2,11 +2,23 @@ package de.fau.amos.virtualledger.server.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.ImplicitGrant;
+import springfox.documentation.service.LoginEndpoint;
+import springfox.documentation.service.OAuth;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +29,30 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("de.fau.amos.virtualledger.server.api"))
                 .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Collections.singletonList(securitySchema()));
+    }
+
+    private OAuth securitySchema() {
+        List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+        authorizationScopeList.add(new AuthorizationScope("multibanking-client", "Multibanking Client"));
+
+        List<GrantType> grantTypes = new ArrayList<>();
+        LoginEndpoint loginEndpoint = new LoginEndpoint("https://multibanking-keycloak.dev.adorsys.de/auth/realms/multibanking/protocol/openid-connect/auth");
+        ImplicitGrant implicitGrant = new ImplicitGrant(loginEndpoint, "access_token");
+        grantTypes.add(implicitGrant);
+
+        return new OAuthBuilder()
+                .name("oauth2")
+                .grantTypes(grantTypes)
+                .scopes(authorizationScopeList)
                 .build();
     }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration(
+                "multibanking-client", "foo", "multibanking", "foo", "foo", ApiKeyVehicle.HEADER, "api_key", ",");
+    }
+
 }
