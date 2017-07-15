987ba69a1400e666921e2b516dfb3d0926fa3f22
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-15 15:53:38 +0200

    Remove userid from urls for new adorsys api

--- server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiUrlProvider.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/banking/adorsys/api/BankingApiUrlProvider.java
@@ -19,21 +19,17 @@ public class BankingApiUrlProvider {
     }
 
     public String getBankAccessEndpointUrl(final String userId) {
-        String url = configuration.getBankingApiUrlAbsolute() + configuration.getBankAccessApiUrlRelative();
-        url = url.replaceAll("\\{userId}", userId);
-        return url;
+        return configuration.getBankingApiUrlAbsolute() + configuration.getBankAccessApiUrlRelative();
     }
 
     public String getBankAccountEndpointUrl(final String userId, final String bankAccessId) {
         String url = configuration.getBankingApiUrlAbsolute() + configuration.getBankAccountApiUrlRelative();
-        url = url.replaceAll("\\{userId}", userId);
         url = url.replaceAll("\\{accessId}", bankAccessId);
         return url;
     }
 
     public String getBankAccountSyncEndpointUrl(final String userId, final String bankAccessId, final String bankAccountId) {
         String url = configuration.getBankingApiUrlAbsolute() + configuration.getBankAccountSyncApiUrlRelative();
-        url = url.replaceAll("\\{userId}", userId);
         url = url.replaceAll("\\{accessId}", bankAccessId);
         url = url.replaceAll("\\{accountId}", bankAccountId);
         return url;
