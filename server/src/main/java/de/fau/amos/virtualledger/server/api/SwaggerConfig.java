bafa81b695f698c0e5e6c9ab864d2c6ce8849ea1
Georg <georg.schwarz@fau.de>
2017-07-16 13:07:02 +0200

    outsourced swagger config parameters to application.properties

--- server/src/main/java/de/fau/amos/virtualledger/server/api/SwaggerConfig.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package de.fau.amos.virtualledger.server.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.OAuthBuilder;
@@ -22,11 +23,31 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+
+    @Value("${swagger.config.apiurl}")
+    private String swaggerConfigApiUrl;
+
+    @Value("${swagger.config.scope.name}")
+    private String swaggerConfigScopeName;
+
+    @Value("${swagger.config.scope.description}")
+    private String swaggerConfigScopeDescription;
+
+    @Value("${swagger.config.tokenurl}")
+    private String swaggerConfigTokenUrl;
+
+    @Value("${swagger.config.clientid}")
+    private String swaggerConfigClientId;
+
+    @Value("${swagger.config.realm}")
+    private String swaggerConfigRealm;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("de.fau.amos.virtualledger.server.api"))
+                .apis(RequestHandlerSelectors.basePackage(swaggerConfigApiUrl))
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(Collections.singletonList(securitySchema()));
@@ -34,10 +55,10 @@ public class SwaggerConfig {
 
     private OAuth securitySchema() {
         List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
-        authorizationScopeList.add(new AuthorizationScope("multibanking-client", "Multibanking Client"));
+        authorizationScopeList.add(new AuthorizationScope(swaggerConfigScopeName, swaggerConfigScopeDescription));
 
         List<GrantType> grantTypes = new ArrayList<>();
-        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant("https://multibanking-keycloak.dev.adorsys.de/auth/realms/multibanking/protocol/openid-connect/token");
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant(swaggerConfigTokenUrl);
         grantTypes.add(grantType);
 
         return new OAuthBuilder()
@@ -50,6 +71,6 @@ public class SwaggerConfig {
     @Bean
     public SecurityConfiguration security() {
         return new SecurityConfiguration(
-                "multibanking-client", "foo", "multibanking", "foo", "foo", ApiKeyVehicle.HEADER, "api_key", ",");
+                swaggerConfigClientId, "foo", swaggerConfigRealm, "foo", "foo", ApiKeyVehicle.HEADER, "api_key", ",");
     }
 }
