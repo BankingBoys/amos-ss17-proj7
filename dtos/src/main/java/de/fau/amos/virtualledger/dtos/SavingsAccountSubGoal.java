ae4b70c447c149ecf6256b95c83d44cc251876ec
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-17 19:56:20 +0200

    Add data object for saving SubGoals

---
+++ dtos/src/main/java/de/fau/amos/virtualledger/dtos/SavingsAccountSubGoal.java
@@ -0,0 +1,23 @@
+package de.fau.amos.virtualledger.dtos;
+
+public class SavingsAccountSubGoal {
+
+    private String name;
+    private String amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(final String amount) {
+        this.amount = amount;
+    }
+}
