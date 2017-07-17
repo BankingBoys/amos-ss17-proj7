40c29d4b0443d243af109531b3c5619479281d5c
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-07-17 19:37:25 +0200

    Create fragment class for AddSavingsAccountAmountSplit view

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountAmountSplitFragment.java
@@ -0,0 +1,13 @@
+package de.fau.amos.virtualledger.android.views.savings.add;
+
+import de.fau.amos.virtualledger.android.model.SavingsAccount;
+
+public class AddSavingsAccountAmountSplitFragment extends AddSavingsAccountPage {
+    @SuppressWarnings("unused")
+    private static final String TAG = AddSavingsAccountAmountSplitFragment.class.getSimpleName();
+
+    @Override
+    void fillInData(final SavingsAccount addSavingsAccountResult) {
+
+    }
+}
