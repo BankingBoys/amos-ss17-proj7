c0d8527c5d64d33488587649dd8cd94afd1a0eb3
Georg <georg.schwarz@fau.de>
2017-07-18 12:18:48 +0200

    added methods to transform into Set<SavingsAccountSubGoalEntity>

--- server/src/main/java/de/fau/amos/virtualledger/server/factories/SavingsAccountEntityTransformer.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/factories/SavingsAccountEntityTransformer.java
@@ -4,9 +4,22 @@ import de.fau.amos.virtualledger.dtos.SavingsAccountSubGoal;
 import de.fau.amos.virtualledger.server.model.SavingsAccountSubGoalEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Component
 public class SavingsAccountEntityTransformer {
 
+
+    public Set<SavingsAccountSubGoalEntity> transformSavingsAccountSubGoalIdentifierIntoEntity(List<SavingsAccountSubGoal> subGoals) {
+
+        Set<SavingsAccountSubGoalEntity> savingsAccountSubGoalEntities = new HashSet<>();
+        for (SavingsAccountSubGoal savingsAccountSubGoal : subGoals) {
+            savingsAccountSubGoalEntities.add(transformSavingsAccountSubGoalIdentifierIntoEntity(savingsAccountSubGoal));
+        }
+        return savingsAccountSubGoalEntities;
+    }
 
     public SavingsAccountSubGoalEntity transformSavingsAccountSubGoalIdentifierIntoEntity(SavingsAccountSubGoal savingsAccountSubGoal) {
 
