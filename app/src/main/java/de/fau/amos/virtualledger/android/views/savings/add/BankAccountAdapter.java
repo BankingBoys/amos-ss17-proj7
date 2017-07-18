145f3ac88ed0db543cb1e5dfcde1d2072fc19b63
simonschmoll <simon.schmoll@gmail.com>
2017-07-18 10:54:47 +0200

    added default implementation for BankAccountAdapter

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/BankAccountAdapter.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/BankAccountAdapter.java
@@ -1,8 +1,22 @@
 package de.fau.amos.virtualledger.android.views.savings.add;
 
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import de.fau.amos.virtualledger.dtos.BankAccount;
+
 /**
  * Created by Simon on 17.07.2017.
  */
 
-public class BankAccountAdapter {
+public class BankAccountAdapter extends ArrayAdapter<BankAccount> {
+
+    private BankAccountSelectedListener bankAccountListener;
+
+    public BankAccountAdapter (Activity activity, int layout, ArrayList<BankAccount> bankAccounts, BankAccountSelectedListener bankAccountListener) {
+        super(activity, layout, bankAccounts);
+        this.bankAccountListener = bankAccountListener;
+    }
 }
