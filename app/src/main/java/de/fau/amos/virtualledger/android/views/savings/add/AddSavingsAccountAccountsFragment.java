37be904c1d1fbf131ff0ffbce9045e0a629e1f30
simonschmoll <simon.schmoll@gmail.com>
2017-07-18 11:22:30 +0200

    injection of BankingDataManager and getting of all bank accounts

--- app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountAccountsFragment.java
+++ app/src/main/java/de/fau/amos/virtualledger/android/views/savings/add/AddSavingsAccountAccountsFragment.java
@@ -1,19 +1,38 @@
 package de.fau.amos.virtualledger.android.views.savings.add;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.fau.amos.virtualledger.R;
+import de.fau.amos.virtualledger.android.dagger.App;
+import de.fau.amos.virtualledger.android.data.BankingDataManager;
+import de.fau.amos.virtualledger.android.data.SyncStatus;
 import de.fau.amos.virtualledger.android.model.SavingsAccount;
+import de.fau.amos.virtualledger.dtos.BankAccess;
+import de.fau.amos.virtualledger.dtos.BankAccount;
 
 public class AddSavingsAccountAccountsFragment extends AddSavingsAccountPage {
     @SuppressWarnings("unused")
     private static final String TAG = AddSavingsAccountAccountsFragment.class.getSimpleName();
+    private BankAccountSelectedListener bankAccountSelectedListener;
+    private BankAccount bankAccount;
+    private BankAccountAdapter adapter;
+
+    @Inject
+    BankingDataManager bankingDataManager;
 
     @OnClick(R.id.add_savings_account_button_accounts)
     public void onClickChooseAccountsButton() {
@@ -23,6 +42,7 @@ public class AddSavingsAccountAccountsFragment extends AddSavingsAccountPage {
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        ((App) getActivity().getApplication()).getNetComponent().inject(this);
         final View view = inflater.inflate(R.layout.saving_accounts_add_fragment_accounts, container, false);
         ButterKnife.bind(this, view);
 
@@ -32,5 +52,26 @@ public class AddSavingsAccountAccountsFragment extends AddSavingsAccountPage {
     @Override
     public void fillInData(final SavingsAccount addSavingsAccountResult) {
         //TODO
+    }
+
+    @NonNull
+    private ArrayList<BankAccount> getBankAccounts() {
+        ArrayList<BankAccount> allBankAccounts = new ArrayList<>();
+        List<BankAccess> accesses = new ArrayList<>();
+        if(bankingDataManager.getSyncStatus() == SyncStatus.SYNCED) {
+            try {
+                accesses  = bankingDataManager.getBankAccesses();
+            } catch (Exception e) {
+                logger().info(TAG + " could not get bank accesses");
+            }
+            for(BankAccess bankAccess: accesses) {
+                allBankAccounts.addAll(bankAccess.getBankaccounts());
+            }
+        }
+        return allBankAccounts;
+    }
+
+    private Logger logger() {
+        return Logger.getLogger(this.getClass().getCanonicalName());
     }
 }
