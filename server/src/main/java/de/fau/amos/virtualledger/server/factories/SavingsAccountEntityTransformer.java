e97bafdf9cb44440e1932e7758fbafb9d29c4002
Georg <georg.schwarz@fau.de>
2017-07-18 12:19:17 +0200

    added method to transform into BankAccountIdentifierEntity

--- server/src/main/java/de/fau/amos/virtualledger/server/factories/SavingsAccountEntityTransformer.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/factories/SavingsAccountEntityTransformer.java
@@ -1,9 +1,17 @@
 package de.fau.amos.virtualledger.server.factories;
 
+import de.fau.amos.virtualledger.dtos.BankAccountIdentifier;
+import de.fau.amos.virtualledger.dtos.Contact;
+import de.fau.amos.virtualledger.dtos.SavingsAccount;
 import de.fau.amos.virtualledger.dtos.SavingsAccountSubGoal;
+import de.fau.amos.virtualledger.server.model.BankAccountIdentifierEntity;
+import de.fau.amos.virtualledger.server.model.SavingsAccountEntity;
 import de.fau.amos.virtualledger.server.model.SavingsAccountSubGoalEntity;
+import de.fau.amos.virtualledger.server.model.SavingsAccountUserRelation;
+import de.fau.amos.virtualledger.server.model.User;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,6 +19,14 @@ import java.util.Set;
 @Component
 public class SavingsAccountEntityTransformer {
 
+
+    public BankAccountIdentifierEntity transformBankAccountIdentifierIntoEntity(BankAccountIdentifier bankAccountIdentifier) {
+
+        return new BankAccountIdentifierEntity(
+                bankAccountIdentifier.getAccessid(),
+                bankAccountIdentifier.getAccountid()
+        );
+    }
 
     public Set<SavingsAccountSubGoalEntity> transformSavingsAccountSubGoalIdentifierIntoEntity(List<SavingsAccountSubGoal> subGoals) {
 
