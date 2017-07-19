b6e045bc0d7af937bf23151593531181c555f882
simonschmoll <simon.schmoll@gmail.com>
2017-07-19 16:45:28 +0200

    changed response from deleting contact (Rest Api)

--- server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/api/ContactsApiEndpoint.java
@@ -72,7 +72,7 @@ public class ContactsApiEndpoint {
     public ResponseEntity<?> deleteContactEndpoint(@PathVariable("contactEmail") String contactEmail) throws ServletException {
         final String username = keycloakUtilizer.getEmail();
         if (username == null || username.isEmpty()) {
-            return new ResponseEntity<>("Authentication failed! Your username wasn't found.", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(stringApiModelFactory.createStringApiModel("Authentication failed! Your username wasn't found."), HttpStatus.FORBIDDEN);
         }
         LOGGER.info("deleteContactEndpoint of " + username + " was requested");
         return this.deleteContact(contactEmail, username);
@@ -97,8 +97,8 @@ public class ContactsApiEndpoint {
         try {
             contactsController.deleteContact(contactEmail, username);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(stringApiModelFactory.createStringApiModel(e.getMessage()), HttpStatus.FORBIDDEN);
         }
-        return new ResponseEntity<>("Deleting of Contact successful", HttpStatus.OK);
+        return new ResponseEntity<>(stringApiModelFactory.createStringApiModel("Deleting of Contact successful"), HttpStatus.OK);
     }
 }
