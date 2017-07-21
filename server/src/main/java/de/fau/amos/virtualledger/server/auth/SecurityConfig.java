ec1ade58ae5c0543650b7cfa9e92b36000f18056
Georg <georg.schwarz@fau.de>
2017-07-21 10:14:32 +0200

    registered UserRegistrationFilter by using Bean-Style, so it works again

--- server/src/main/java/de/fau/amos/virtualledger/server/auth/SecurityConfig.java
+++ server/src/main/java/de/fau/amos/virtualledger/server/auth/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +17,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
@@ -34,17 +34,25 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+        http.csrf().disable();
         http
                 .authorizeRequests()
                 .antMatchers("/api/banking/**").permitAll()
                 .antMatchers("/api/savings/**").permitAll()
                 .antMatchers("/api/contacts/**").permitAll();
-        http
-                .addFilterAfter(new UserRegistrationFilter(keycloakUtilizer, authenticationController), FilterSecurityInterceptor.class)
-                .antMatcher("/api/banking/**")
-                .antMatcher("/api/savings/**")
-                .antMatcher("/api/contacts/**");
-        http.csrf().disable();
+    }
+
+    @Bean
+    public FilterRegistrationBean userRegistrationFilter() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new UserRegistrationFilter(keycloakUtilizer, authenticationController));
+        registration.addUrlPatterns("/api/banking/*");
+        registration.addUrlPatterns("/api/savings/*");
+        registration.addUrlPatterns("/api/contacts/*");
+        registration.setName("UserRegistrationFilter");
+        registration.setOrder(100);
+        return registration;
     }
 
     @Autowired
