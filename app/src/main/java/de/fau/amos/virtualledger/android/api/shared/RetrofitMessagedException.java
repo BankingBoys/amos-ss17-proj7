4e59de9f8a0c1470f95810abb3a3cb59b850e17f
Georg <georg.schwarz@fau.de>
2017-07-24 13:23:04 +0200

    created RetrofitMessagedException in order to distinguish unsuccessful requests with and withouht containing error message

---
+++ app/src/main/java/de/fau/amos/virtualledger/android/api/shared/RetrofitMessagedException.java
@@ -0,0 +1,13 @@
+package de.fau.amos.virtualledger.android.api.shared;
+
+/**
+ * Created by Georg on 24.07.2017.
+ */
+
+public class RetrofitMessagedException extends Throwable {
+
+    public RetrofitMessagedException(String message) {
+        super(message);
+    }
+
+}
